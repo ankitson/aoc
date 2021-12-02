e14419af148dc515826ceff5e665445171542c24
Ankit Soni <ankitsoni9@gmail.com>
2021-12-02 04:40:47 +0000

    generalize window count

--- src/aoc/day1.rs
+++ src/aoc/day1.rs
@@ -1,41 +1,20 @@
 fn parse() -> Vec<i32> {
     let contents: &str = include_str!("../../input/day1.txt");
-    contents
-        .split('\n')
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect()
+    contents.split('\n').map(|x| x.parse::<i32>().unwrap()).collect()
+}
+
+fn count_window(input: Vec<i32>, window_size: usize) -> i32 {
+    let mut count = 0;
+    for i in 0..input.len() - window_size {
+        count += if input[i + window_size] > input[i] { 1 } else { 0 };
+    }
+    count
 }
 
 pub fn part1() -> String {
-    let nums = parse();
-
-    let mut i = 1;
-    let mut prev: i32 = nums[0];
-    let mut incr = 0;
-    while i < nums.len() {
-        if nums[i] > prev {
-            incr += 1;
-        }
-        prev = nums[i];
-        i += 1;
-    }
-    incr.to_string()
+    count_window(parse(), 1).to_string()
 }
 
 pub fn part2() -> String {
-    let nums = parse();
-
-    let mut iter = nums.windows(3);
-    let mut prev: i32 = iter.next().unwrap().iter().sum();
-    let mut incr = 0;
-
-    for window in iter {
-        let sum: i32 = window.iter().sum();
-        if sum > prev {
-            incr += 1
-        }
-        prev = sum;
-    }
-
-    incr.to_string()
+    count_window(parse(), 3).to_string()
 }
