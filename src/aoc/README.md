f200d966113c340c62f876dbc14924b9bbfe4373
Ankit Soni <ankitsoni9@gmail.com>
2021-12-02 04:20:44 +0000

    README

---
+++ src/aoc/README.md
@@ -0,0 +1,4 @@
+# Solutions to Advent of Code 2021
+
+My goal here is to use AoC to learn how to write idiomatic Rust
+
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod aoc;
 
 fn main() {
     println!("Hello, world!");
+
     println!("---Day 1---");
     println!("{}", aoc::day1::part1()); //1387
     println!("{}", aoc::day1::part2()); //1362
