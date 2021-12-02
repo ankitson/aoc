a963425a0b3b6ff1dc92f6242113a1e9e096d19a
Ankit Soni <ankitsoni9@gmail.com>
2021-12-02 03:42:58 +0000

    Day 1 Part 2

--- src/aoc/day1.rs
+++ src/aoc/day1.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 fn parse() -> Vec<i32> {
     let path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("input")
-        .join("day1_part1.txt");
+        .join("day1.txt");
     let contents = fs::read_to_string(path).expect("Could not read file");
     let lines: Vec<i32> = contents
         .split('\n')
@@ -13,7 +13,7 @@ fn parse() -> Vec<i32> {
     lines
 }
 
-pub fn part1() -> i32 {
+pub fn part1() -> String {
     let nums = parse();
 
     let mut i = 1;
@@ -26,5 +26,23 @@ pub fn part1() -> i32 {
         prev = nums[i];
         i += 1;
     }
-    incr
+    incr.to_string()
+}
+
+pub fn part2() -> String {
+    let nums = parse();
+
+    let mut iter = nums.windows(3);
+    let mut prev: i32 = iter.next().unwrap().iter().sum();
+    let mut incr = 0;
+
+    for window in iter {
+        let sum: i32 = window.iter().sum();
+        if sum > prev {
+            incr += 1
+        }
+        prev = sum;
+    }
+
+    incr.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     println!("Hello, world!");
     println!("---Day 1---");
     println!("{}", aoc::day1::part1()); //1387
-    
+    println!("{}", aoc::day1::part2()); //1362
 }
