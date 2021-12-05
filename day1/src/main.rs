6dd8473cdad895cce52c03ae3efb706f5cfa53ce
Ankit Soni <ankitsoni9@gmail.com>
2021-12-05 02:48:23 +0000

    reproducible case - but not identical error

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,9 +3,32 @@ mod soln;
 /*
    BROKEN: Why does this not compile?
 */
-pub fn main() {
-    println!("Hello World");
-    let contents: &str = include_str!("../day1.txt");
-    let soln = soln::Soln1 {};
-    soln.parse(contents);
+// pub fn main() {
+//     println!("Hello World");
+//     let contents: &str = include_str!("../day1.txt");
+//     let soln = soln::Soln1;
+//     soln.parse(contents);
+// }
+
+// reproducible case
+fn main() {
+    trait Woofer {
+        fn bark(tree: String) -> String;
+    }
+
+    struct Dog {}
+    impl Dog {
+        fn constant_bark() -> String {
+            "grrrr".to_string()
+        }
+    }
+
+    impl Woofer for Dog {
+        fn bark(tree: String) -> String {
+            Self::constant_bark()
+        }
+    }
+
+    let d = Dog {};
+    d.bark("pine");
 }
