c6e8599f3e4f3b8b872210fee6290d421068aeed
Ankit Soni <ankitsoni9@gmail.com>
2021-12-05 02:20:36 +0000

    fix1 trait ownership

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,22 +1,24 @@
 use std::fmt::Display;
 
 /*
-    BROKEN: This implementation does not compile because the returned string slice has an unknown lifetime
+    FIX1: This returns Strings, meaning the string allocated is moved to the caller
+
+    This compiles, but what if say the output type is a String, and unparse should return a slice of that string only?
 */
 trait Solution<InputType, OutputType> {
     fn parse(input: &str) -> InputType;
 
-    fn unparse(output: OutputType) -> &str;
+    fn unparse(output: OutputType) -> String;
 
     fn part1_core(input: InputType) -> OutputType;
 
     fn part2_core(input: InputType) -> OutputType;
 
-    fn part1(input: &str) -> &str {
+    fn part1(input: &str) -> String {
         Self::unparse(Self::part1_core(Self::parse(input)))
     }
 
-    fn part2(input: &str) -> &str {
+    fn part2(input: &str) -> String {
         Self::unparse(Self::part2_core(Self::parse(input)))
     }
 }
