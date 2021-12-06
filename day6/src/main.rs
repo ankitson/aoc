e11f09abe3f5e48375a19a204199e8117723ff9f
Ankit Soni <ankitsoni9@gmail.com>
2021-12-06 05:45:39 +0000

    day 6 part 1 works

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,8 +2,8 @@ mod soln;
 
 pub fn main() {
     let contents: &str = include_str!("../inputs/day6.txt");
-    // let part1 = soln::Soln1::part1(contents, 1000);
-    // println!("{:?}", part1);
+    let part1 = soln::Soln1::part1(contents, 80);
+    println!("{:?}", part1);
     // let part2 = soln::Soln1::part2(contents, 1000);
     // println!("{:?}", part2);
 }
@@ -15,7 +15,9 @@ mod tests {
     #[test]
     fn test_part1() {
         let sample: &str = include_str!("../inputs/sample6.txt");
-        let soln = soln::Soln1::part1(sample, 10);
-        assert_eq!(soln, 5);
+        let soln = soln::Soln1::part1(sample, 18);
+        assert_eq!(soln, 26);
+        let soln = soln::Soln1::part1(sample, 80);
+        assert_eq!(soln, 5934);
     }
 }
--- day6/src/soln.rs
+++ day6/src/soln.rs
@@ -2,4 +2,26 @@ use shared;
 use std::{fmt::Debug, iter, ops::Range};
 
 pub struct Soln1 {}
-impl Soln1 {}
+impl Soln1 {
+    pub fn part1(input: &str, days: usize) -> usize {
+        // 3,4,3,1,2
+        let fish = input.trim().split(',').map(|x| x.parse::<u8>().expect("illegal age"));
+        let mut fish = fish.collect::<Vec<u8>>();
+        for _ in 0..days {
+            Self::advance(&mut fish);
+        }
+        println!("Final state: {:?}", fish);
+        fish.len()
+    }
+
+    fn advance(fish: &mut Vec<u8>) -> () {
+        for i in 0..fish.len() {
+            if fish[i] == 0 {
+                fish[i] = 6;
+                fish.push(8);
+            } else {
+                fish[i] -= 1;
+            }
+        }
+    }
+}
