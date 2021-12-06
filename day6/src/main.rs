941d73cb7427ab542f5150b77d95fa0b46f45cb0
Ankit Soni <ankitsoni9@gmail.com>
2021-12-06 06:15:23 +0000

    day 6 part 2 done!

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,6 +4,8 @@ pub fn main() {
     let contents: &str = include_str!("../inputs/day6.txt");
     let part1 = soln::Soln1::part1(contents, 80);
     println!("{:?}", part1);
+    let part2 = soln::Soln1::part2(contents, 256);
+    println!("{:?}", part2);
     // let part2 = soln::Soln1::part2(contents, 1000);
     // println!("{:?}", part2);
 }
@@ -18,6 +20,10 @@ mod tests {
         let soln = soln::Soln1::part1(sample, 18);
         assert_eq!(soln, 26);
         let soln = soln::Soln1::part1(sample, 80);
+        assert_eq!(soln, 5934);
+        let soln = soln::Soln1::part2(sample, 18);
+        assert_eq!(soln, 26);
+        let soln = soln::Soln1::part2(sample, 80);
         assert_eq!(soln, 5934);
     }
 }
--- day6/src/soln.rs
+++ day6/src/soln.rs
@@ -1,17 +1,30 @@
-use shared;
-use std::{fmt::Debug, iter, ops::Range};
-
 pub struct Soln1 {}
 impl Soln1 {
     pub fn part1(input: &str, days: usize) -> usize {
-        // 3,4,3,1,2
         let fish = input.trim().split(',').map(|x| x.parse::<u8>().expect("illegal age"));
         let mut fish = fish.collect::<Vec<u8>>();
         for _ in 0..days {
             Self::advance(&mut fish);
         }
-        println!("Final state: {:?}", fish);
+        // println!("Final state: {:?}", fish);
         fish.len()
+    }
+
+    pub fn part2(input: &str, days: usize) -> u64 {
+        let fish = input.trim().split(',').map(|x| x.parse::<u8>().expect("illegal age"));
+        let mut by_age: [u64; 9] = [0; 9];
+        for age in fish {
+            by_age[usize::from(age)] += 1
+        }
+        //fish: 3,4,3,1,2
+        //ba: [0,1,1,2,1,0,0,0,0]
+        //ba: [1,1,2,1,0,0,0,0,0] == 0,1,2,2,3
+        //ba: [1,2,1,0,0,0,1,0,1] == 0,1,1,2,6,8
+        for day in 0..days {
+            by_age[7] += by_age[0];
+            by_age.rotate_left(1);
+        }
+        by_age.iter().sum()
     }
 
     fn advance(fish: &mut Vec<u8>) -> () {
