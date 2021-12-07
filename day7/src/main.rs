52271c108f89dc3b54e1b4da226544986540ae31
Ankit Soni <ankitsoni9@gmail.com>
2021-12-07 12:50:06 +0000

    day 7 better solns

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(drain_filter)]
 mod soln;
 
 pub fn main() {
@@ -5,6 +6,10 @@ pub fn main() {
     let part1 = soln::Soln1::part1(contents);
     println!("{:?}", part1);
     let part2 = soln::Soln1::part2(contents);
+    println!("{:?}", part2);
+    let part1 = soln::Soln1::part1_fast(contents);
+    println!("{:?}", part1);
+    let part2 = soln::Soln1::part2_fast(contents);
     println!("{:?}", part2);
 }
 
@@ -17,6 +22,24 @@ mod tests {
         let sample: &str = include_str!("../inputs/sample7.txt");
         let soln = soln::Soln1::part1(sample);
         assert_eq!(soln, 37);
+
+        let input: &str = include_str!("../inputs/day7.txt");
+        let soln = soln::Soln1::part1(input);
+        assert_eq!(soln, 337488);
+    }
+
+    #[test]
+    fn test_quickselect() {
+        assert_eq!(soln::Soln1::median_quickselect(2, vec![1, 2, 3, 4, 5]), 3);
+        assert_eq!(soln::Soln1::median_quickselect(1, vec![1, 2, 3, 4, 5]), 2);
+        assert_eq!(soln::Soln1::median_quickselect(1, vec![1, 2, 2, 4]), 2);
+    }
+
+    #[test]
+    fn test_part1_fast() {
+        let sample: &str = include_str!("../inputs/sample7.txt");
+        let soln = soln::Soln1::part1_fast(sample);
+        assert_eq!(soln, 37);
     }
 
     #[test]
@@ -24,5 +47,20 @@ mod tests {
         let sample: &str = include_str!("../inputs/sample7.txt");
         let soln = soln::Soln1::part2(sample);
         assert_eq!(soln, 168);
+
+        let input: &str = include_str!("../inputs/day7.txt");
+        let soln = soln::Soln1::part2(input);
+        assert_eq!(soln, 89647695);
+    }
+
+    #[test]
+    fn test_part2_fast() {
+        let sample: &str = include_str!("../inputs/sample7.txt");
+        let soln = soln::Soln1::part2_fast(sample);
+        assert_eq!(soln, 168);
+
+        let input: &str = include_str!("../inputs/day7.txt");
+        let soln = soln::Soln1::part2_fast(input);
+        assert_eq!(soln, 89647695);
     }
 }
