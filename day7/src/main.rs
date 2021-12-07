707fef12452f08db66bc4f787eea7bcac2536e3c
Ankit Soni <ankitsoni9@gmail.com>
2021-12-07 05:40:54 +0000

    day 7 part 2 done

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,10 +4,8 @@ pub fn main() {
     let contents: &str = include_str!("../inputs/day7.txt");
     let part1 = soln::Soln1::part1(contents);
     println!("{:?}", part1);
-    // let part2 = soln::Soln1::part2(contents, 256);
-    // println!("{:?}", part2);
-    // let part2 = soln::Soln1::part2(contents, 1000);
-    // println!("{:?}", part2);
+    let part2 = soln::Soln1::part2(contents);
+    println!("{:?}", part2);
 }
 
 #[cfg(test)]
@@ -19,13 +17,12 @@ mod tests {
         let sample: &str = include_str!("../inputs/sample7.txt");
         let soln = soln::Soln1::part1(sample);
         assert_eq!(soln, 37);
+    }
 
-        // assert_eq!(soln, 26);
-        // let soln = soln::Soln1::part1(sample, 80);
-        // assert_eq!(soln, 5934);
-        // let soln = soln::Soln1::part2(sample, 18);
-        // assert_eq!(soln, 26);
-        // let soln = soln::Soln1::part2(sample, 80);
-        // assert_eq!(soln, 5934);
+    #[test]
+    fn test_part2() {
+        let sample: &str = include_str!("../inputs/sample7.txt");
+        let soln = soln::Soln1::part2(sample);
+        assert_eq!(soln, 168);
     }
 }
