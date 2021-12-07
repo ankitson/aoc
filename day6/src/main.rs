99a464e0d9720473948bb82d06441f631f30f961
Ankit Soni <ankitsoni9@gmail.com>
2021-12-07 03:55:12 +0000

    matrix mult method WIP

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -8,11 +8,15 @@ pub fn main() {
     println!("{:?}", part2);
     // let part2 = soln::Soln1::part2(contents, 1000);
     // println!("{:?}", part2);
+
+    // let polysoln = soln::Soln1::part2_faster(contents, 80);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::soln;
+    use std::ops::Mul;
+
+    use crate::soln::{self, SquareMatrix};
 
     #[test]
     fn test_part1() {
@@ -25,5 +29,14 @@ mod tests {
         assert_eq!(soln, 26);
         let soln = soln::Soln1::part2(sample, 80);
         assert_eq!(soln, 5934);
+    }
+
+    #[test]
+    fn test_mat_multiply() {
+        let m1 = SquareMatrix::<3>([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        let m2 = SquareMatrix::<3>([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        let m3 = m1.mul(m2);
+        let expected = SquareMatrix::<3>([[30, 36, 42], [66, 81, 96], [102, 126, 150]]);
+        assert_eq!(m3, expected);
     }
 }
--- day6/src/soln.rs
+++ day6/src/soln.rs
@@ -1,5 +1,53 @@
+use std::ops::Mul;
+
 pub struct Soln1 {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SquareMatrix<const N: usize>(pub [[u8; N]; N]);
+impl<const N: usize> Mul<SquareMatrix<N>> for SquareMatrix<N> {
+    type Output = SquareMatrix<N>;
+
+    fn mul(self, rhs: SquareMatrix<N>) -> Self::Output {
+        let mut result = SquareMatrix([[0; N]; N]);
+        for i in 0..N {
+            for j in 0..N {
+                for k in 0..N {
+                    result.0[i][j] += self.0[i][k] * rhs.0[k][j];
+                }
+            }
+        }
+        result
+    }
+}
+
+//PROBLEM: cant recurse in impl fn?
+impl<const N: usize> SquareMatrix<N> {
+    fn exponent(m: SquareMatrix<N>, p: u8) -> Self {
+        if (p == 1) {
+            m
+        } else {
+            exponent(m, p / 2).mul(exponent(m, p / 2))
+        }
+    }
+}
+
 impl Soln1 {
+    const matrix: SquareMatrix<9> = SquareMatrix([
+        [0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0],
+    ]);
+    // const N: u8 = 9;
+    // fn bla(a: [u8; N]) -> [u8; N] {
+    // a
+    // }
+
     pub fn part1(input: &str, days: usize) -> usize {
         let fish = input.trim().split(',').map(|x| x.parse::<u8>().expect("illegal age"));
         let mut fish = fish.collect::<Vec<u8>>();
@@ -16,16 +64,19 @@ impl Soln1 {
         for age in fish {
             by_age[usize::from(age)] += 1
         }
-        //fish: 3,4,3,1,2
-        //ba: [0,1,1,2,1,0,0,0,0]
-        //ba: [1,1,2,1,0,0,0,0,0] == 0,1,2,2,3
-        //ba: [1,2,1,0,0,0,1,0,1] == 0,1,1,2,6,8
+
+        let before = by_age;
         for _ in 0..days {
             by_age[7] += by_age[0];
             by_age.rotate_left(1);
+            let after = by_age;
         }
         by_age.iter().sum()
     }
+
+    // fn mat_multiply(a: [[u8; N]; N], b: [[u8; N]; N]) -> [[u8; N]; N] {
+    // a
+    // }
 
     fn advance(fish: &mut Vec<u8>) -> () {
         for i in 0..fish.len() {
