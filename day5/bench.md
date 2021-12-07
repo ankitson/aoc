dc23de7c1213791b10a8903249db0e4db6b28b03
Ankit Soni <ankitsoni9@gmail.com>
2021-12-07 13:32:44 +0000

    cleanup benchmarking setup

--- day5/bench.md
+++ day5/bench.md
@@ -2,7 +2,10 @@
 
 Using dhat (memory allocation) + criterion (cpu time)
 
-Run `bench.sh` after committing
+Run `bench.sh` on a clean git tree. It aborts if the git tree is not clean - this ensures every benchmark result is tied to a particular set of code.
 
+`bench.sh` will run dhat and criterion and save the results to bench_results/<git_commit_id>
+
+`dhat` runs only when the allocator is added to the code. For now, this script does not automatically add/remove code to switch to the dhat heap.
 
 
--- day5/bench.sh
+++ day5/bench.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-$(git diff-index --quiet HEAD --);
+git diff-index --quiet HEAD --;
 GIT_IS_CLEAN=$?;
 GIT_COMMIT=$(git rev-parse --short HEAD);
 
-if [ $GIT_IS_CLEAN -ne 0 ]; then
-  echo "git staging is dirty";
-#  exit 1;
-fi;
-
-if [ $(cargo build --release) -ne 0]; then
+cargo build --release
+BUILD_STATUS=$?;
+if [ $BUILD_STATUS -ne 0 ]; then
   echo "cannot build project";
   exit 1;
 fi;
 
-echo "Benching $GIT_COMMIT";
+if [ "$1" = "save" ]; then
+  if [ $GIT_IS_CLEAN -ne "0" ]; then
+    echo "git staging is dirty";
+    exit 1;
+  fi;
 
-mkdir -p "bench_results/${GIT_COMMIT}/criterion/";
+  echo "Benching $GIT_COMMIT";
+  echo "Saving results at bench_results/${GIT_COMMIT}/";
+  mkdir -p "bench_results/${GIT_COMMIT}/criterion/";
+fi;
 
 echo "Running cargo bench (criterion)...";
-cargo bench
-cp -R ../target/criterion/day5*/ "bench_results/${GIT_COMMIT}/criterion/"
+cargo -q bench
 
+if [ "$1" = "save" ]; then
+  cp -R ../target/criterion/day5*/ "bench_results/${GIT_COMMIT}/criterion/"
+fi;
 
 echo "Running release binary (dhat)...";
 cargo run --release
-mv dhat-heap.json "bench_results/${GIT_COMMIT}/dhat-heap.json";
+if [ "$1" = "save" ]; then
+  mv dhat-heap.json "bench_results/${GIT_COMMIT}/dhat-heap.json";
 
-echo "Results ready at bench_results/${GIT_COMMIT}/";
+  echo "Results ready at bench_results/${GIT_COMMIT}/";
+fi;
+
 
 #echo "Adding to current commit";
 #git add "bench_results/${GIT_COMMIT}";
