eebb1da4d7c4993ff754dd65edfa749e75e99129
Ankit Soni <ankitsoni9@gmail.com>
2021-12-08 05:17:00 +0000

    day 8 start

---
+++ day8/benches/corebench.rs
@@ -0,0 +1,10 @@
+#![feature(drain_filter)]
+use criterion::{criterion_group, criterion_main, Criterion};
+include!("../src/soln.rs");
+
+pub fn benchmarks(c: &mut Criterion) {
+    let contents: &str = include_str!("../inputs/day8.txt");
+}
+
+criterion_group!(benches, benchmarks);
+criterion_main!(benches);
---
+++ day8/src/main.rs
@@ -0,0 +1,25 @@
+#![feature(drain_filter)]
+mod soln;
+
+pub fn main() {
+    println!("Hello Day 8!");
+    let contents: &str = include_str!("../inputs/day8.txt");
+    // let part1 = soln::Soln1::part1(contents);
+    // println!("Part 1: {:?}", part1);
+    // let part2 = soln::Soln1::part2(contents);
+    // println!("Part 2: {:?}", part2);
+    // let part1 = soln::Soln1::part1_fast(contents);
+    // println!("Part 1 (quickselect+median): {:?}", part1);
+    // let part2 = soln::Soln1::part2_fast(contents);
+    // println!("Part 2 (mean): {:?}", part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::soln;
+
+    #[test]
+    fn test_part1() {
+        let sample: &str = include_str!("../inputs/sample8.txt");
+    }
+}
