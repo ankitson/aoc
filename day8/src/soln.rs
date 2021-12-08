5b535a48a76dd1833d2f28279990bca1fe366709
Ankit Soni <ankitsoni9@gmail.com>
2021-12-08 12:52:10 +0000

    wip constraint prop

--- day8/src/soln.rs
+++ day8/src/soln.rs
@@ -3,7 +3,6 @@ use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
 
 pub struct Soln1 {}
-
 impl Soln1 {
     fn parse(input: &str) -> Vec<(Vec<&str>, Vec<&str>)> {
         let lines = input.trim().split('\n').collect::<Vec<&str>>();
@@ -115,10 +114,100 @@ impl Soln1 {
                 wordc.sort_unstable();
                 let word: String = wordc.into_iter().collect();
                 let digit = maxHitMap.get(&word).unwrap();
-                lineDecode.push((*digit as u8 + '0' as u8) as char) 
+                lineDecode.push((*digit as u8 + '0' as u8) as char)
             }
             decodedLines.push(lineDecode.parse::<usize>().unwrap());
         }
         decodedLines.iter().sum()
+    }
+}
+
+/*
+ * Constraint propagation idea:
+ * cgdf eagcbf fc adefg eacdb fbedga geafcd efc dacfe fdgaecb | dcefbag dgcf fc daefc
+ *
+ * 4 is the only number with 4 segs
+ * we know cgdf = {bcdf}
+ *
+ * representation of constraints:
+ * c1 = map(
+ *  c -> set(b,c,d,f),
+ *  g -> set(b,c,d,f),
+ *  d -> set(b,c,d,f),
+ *  f -> set(b,c,d,f),
+ *  a -> set(a,...,f),
+ *  b -> set(a,...,f)
+ * )
+ *
+ * now process eagcbf
+ * eacgbf = 6 or 9
+ * i.e eagcbf = {abdefg,abcdfg}
+ * i.e eagcbf = {abcdefg}
+ * this constraint map just allows any char to map to any char
+ * call it cA, the open map
+ * c2 = intersect(cA, c2) = c2
+*/
+pub struct Soln2 {
+    // type ConstraintSet: HashSet<char>;
+// type ConstraintMap: HashMap<char, ConstraintSet>;
+}
+impl Soln2 {
+    // type ConstraintSet;
+    // type ConstraintMap;
+    pub fn part2(input: &str) -> usize {
+        let lines = Soln1::parse(input);
+
+        let digits: HashMap<&str, usize> = HashMap::from_iter([
+            ("abcefg", 0),
+            ("cf", 1),
+            ("acdeg", 2),
+            ("acdfg", 3),
+            ("bcdf", 4),
+            ("abdfg", 5),
+            ("abdefg", 6),
+            ("acf", 7),
+            ("abcdefg", 8),
+            ("abcdfg", 9),
+        ]);
+
+        type ConstraintSet = HashSet<char>;
+        type ConstraintMap = HashMap<char, ConstraintSet>;
+        let chars: Vec<char> = ('a'..='g').into_iter().collect();
+        let open_set: HashSet<char> = HashSet::from_iter(chars);
+        let mut open_map: HashMap<char, ConstraintSet> = HashMap::new();
+        for char in open_set.iter() {
+            open_map.insert(*char, open_set.clone());
+        }
+        let open_map = open_map;
+
+        for (words, to_decode) in lines {
+            let mut constraints = open_map.clone();
+            for word in words {
+                // let new_constraints =
+            }
+        }
+        5
+    }
+
+    //TODO: cant define the type alias for the entire impl?
+    fn merge_maps(m1: &mut HashMap<char, HashSet<char>>, m2: HashMap<char, HashSet<char>>) {
+        for (char, allowed_vals) in m1 {
+            if (m2.contains_key(char)) {
+                // let merge: HashSet<char> = allowed_vals & m2.get(char).unwrap();
+
+                //TODO: Doesnt work??
+                // let merge: HashSet<char> = allowed_vals
+                // .intersection(&m2.get(char).cloned().unwrap())
+                // .into_iter()
+                // .collect();
+                let merge: HashSet<char> = allowed_vals
+                    .intersection(&m2.get(char).cloned().unwrap())
+                    .into_iter()
+                    .cloned()
+                    .collect();
+                
+            }
+            // *m1.insert()
+        }
     }
 }
