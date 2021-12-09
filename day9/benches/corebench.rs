ebd6f9e29d7b9a94eae61477efbe3cd7847ecfe7
Ankit Soni <ankitsoni9@gmail.com>
2021-12-09 06:55:23 +0000

    mutating implementation (2.5ms -> 900us)

--- day9/benches/corebench.rs
+++ day9/benches/corebench.rs
@@ -8,6 +8,7 @@ pub fn benchmarks(c: &mut Criterion) {
     let mut group = c.benchmark_group("day9");
     group.bench_function("part1", |b| b.iter(|| Soln1::part1(contents)));
     group.bench_function("part2", |b| b.iter(|| Soln1::part2(contents)));
+    group.bench_function("part2-mut", |b| b.iter(|| Soln1::part2_mut(contents)));
     group.finish();
 }
 
--- day9/src/soln1.rs
+++ day9/src/soln1.rs
@@ -20,6 +20,25 @@ impl Soln1 {
         visited
     }
 
+    fn mut_bfs(grid: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
+        let mut to_visit = vec![(x, y)];
+        let mut size: usize = 0;
+        while to_visit.len() > 0 {
+            let (visitx, visity) = to_visit.remove(0);
+            let nbrs = Self::nbrs(visitx, visity, grid);
+            for (nx, ny) in nbrs {
+                if grid[nx][ny] > grid[visitx][visity] && grid[nx][ny] != 9 && grid[nx][ny] != 10 {
+                    to_visit.push((nx, ny));
+                }
+            }
+            if (grid[visitx][visity] != 10) {
+                size += 1;
+            }
+            grid[visitx][visity] = 10;
+        }
+        size
+    }
+
     pub fn part1(input: &str) -> u32 {
         let heights = shared::parse(input);
 
@@ -43,7 +62,7 @@ impl Soln1 {
     }
 
     pub fn part2(input: &str) -> Vec<usize> {
-        let heights = shared::parse(input);
+        let mut heights = shared::parse(input);
 
         let mut islands: Vec<usize> = Vec::new();
         for i in 0..heights.len() {
@@ -57,7 +76,34 @@ impl Soln1 {
                     }
                 }
                 if lower {
-                    let island = Self::bfs(&heights, i, j).len();
+                    let island = Self::bfs(&mut heights, i, j);
+                    islands.push(island.len());
+                }
+            }
+        }
+        islands.sort_unstable();
+        let mut islands: Vec<usize> = islands.iter().rev().cloned().collect();
+        assert!(islands.len() >= 3);
+        islands.resize(3, 0);
+        islands
+    }
+
+    pub fn part2_mut(input: &str) -> Vec<usize> {
+        let mut heights = shared::parse(input);
+
+        let mut islands: Vec<usize> = Vec::new();
+        for i in 0..heights.len() {
+            for j in 0..heights[0].len() {
+                let nbr = Self::nbrs(i, j, &heights);
+                let pt = heights[i][j];
+                let mut lower = true;
+                for (nx, ny) in nbr {
+                    if (pt >= heights[nx][ny]) {
+                        lower = false;
+                    }
+                }
+                if lower {
+                    let island = Self::mut_bfs(&mut heights, i, j);
                     islands.push(island);
                 }
             }
