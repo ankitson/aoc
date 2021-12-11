12dd9bceccf123160e1f3371f759a6e8a8d119b3
Ankit Soni <ankitsoni9@gmail.com>
2021-12-11 06:48:16 +0000

    fast check all flashed

--- day11/src/soln1.rs
+++ day11/src/soln1.rs
@@ -7,35 +7,35 @@ impl Soln1 {
         let mut grid = shared::parse(input);
         let mut flashes = 0;
         for _ in 0..nsteps {
-            flashes += Self::step(&mut grid).0;
-            Self::print_grid(&grid);
+            flashes += Self::step(&mut grid);
         }
         flashes
     }
 
     pub fn part2(input: &str) -> Option<usize> {
         let mut grid = shared::parse(input);
-        let mut flashes = 0;
+        let nrows: u64 = grid.len().try_into().unwrap();
+        let ncols: u64 = grid[0].len().try_into().unwrap();
+
         let mut i = 0;
-        while true {
-            let allflash = Self::step(&mut grid).1;
-            if (allflash) {
+        loop {
+            let step_flashes = Self::step(&mut grid);
+            if (step_flashes == nrows * ncols) {
                 return Some(i + 1);
             }
             i += 1;
         }
-        return None;
+        unreachable!()
     }
 
-    fn step(grid: &mut Vec<Vec<u32>>) -> (u64, bool) {
+    fn step(grid: &mut Vec<Vec<u32>>) -> u64 {
         for i in 0..grid.len() {
             for j in 0..grid[0].len() {
                 grid[i][j] += 1
             }
         }
 
-        let mut flashed: Vec<Vec<u32>> =
-            vec![vec![0; grid[0].len()]; grid.len()];
+        let mut flashed: Vec<Vec<u32>> = vec![vec![0; grid[0].len()]; grid.len()];
         let mut quiet = false;
         let mut nflashes: u64 = 0;
         while !quiet {
@@ -56,21 +56,7 @@ impl Soln1 {
                 }
             }
         }
-
-        // println!("step after");
-        // Self::print_grid(&grid);
-        // println!("flashed after");
-        // Self::print_grid(&flashed);
-        let mut all_flash = true;
-        for i in 0..grid.len() {
-            for j in 0..grid[i].len() {
-                if flashed[i][j] == 0 {
-                    all_flash = false;
-                }
-            }
-        }
-
-        (nflashes, all_flash)
+        nflashes
     }
 
     fn print_grid(grid: &Vec<Vec<u32>>) {
@@ -86,38 +72,25 @@ impl Soln1 {
     fn nbrs(x: usize, y: usize, hts: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
         let max_x = hts.len();
         let max_y = hts[0].len();
-        let mut nbrs: Vec<(i64, i64)> = Vec::new();
-        nbrs = vec![
+        let cands = vec![
             (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() - 1),
-            (
-                i64::try_from(x).unwrap() - 1,
-                i64::try_from(y).unwrap() + 0i64,
-            ),
+            (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() + 0),
             (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() + 1),
-            (
-                i64::try_from(x).unwrap() + 0i64,
-                i64::try_from(y).unwrap() - 1,
-            ), /*(xy    */
-            (
-                i64::try_from(x).unwrap() + 0i64,
-                i64::try_from(y).unwrap() + 1,
-            ),
+            (i64::try_from(x).unwrap() + 0, i64::try_from(y).unwrap() - 1),
+            (i64::try_from(x).unwrap() + 0, i64::try_from(y).unwrap() + 1),
             (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() - 1),
-            (
-                i64::try_from(x).unwrap() + 1,
-                i64::try_from(y).unwrap() + 0i64,
-            ),
+            (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() + 0),
             (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() + 1),
         ];
 
         let mut nbrs2: Vec<(usize, usize)> = Vec::new();
-        for (a, b) in nbrs {
-            if (a < 0 || b < 0) {
+        for (a, b) in cands {
+            if a < 0 || b < 0 {
                 continue;
             }
             let aa = a.try_into().unwrap();
             let bb = b.try_into().unwrap();
-            if (aa < max_x && bb < max_y) {
+            if aa < max_x && bb < max_y {
                 nbrs2.push((aa, bb));
             }
         }
