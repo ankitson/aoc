69b5cd6ecc09ac77bb0a4810c2beea130752116a
Ankit Soni <ankitsoni9@gmail.com>
2021-12-13 11:47:36 +0000

    day 12 part 1 - backtracking

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,12 +1,12 @@
 // #![feature(array_zip)]
-mod shared;
-mod soln1;
+pub mod shared;
+pub mod soln1;
 
 pub fn main() {
     println!("Hello Day 12!");
-    // let contents: &str = include_str!("../inputs/day12.txt");
-    // let part1 = soln1::Soln1::part1(contents, 100);
-    // println!("Part 1 = {:?}", part1);
+    let contents: &str = include_str!("../inputs/day12.txt");
+    let part1 = soln1::Soln1::part1(contents);
+    println!("Part 1 = {:?}", part1);
     // let part2 = soln1::Soln1::part2(contents);
     // println!("Part 2 = {:?}", part2);
 }
@@ -17,15 +17,20 @@ mod tests {
     use crate::soln1;
 
     #[test]
-    fn test_sample() {
-        let contents: &str = include_str!("../inputs/sample.txt");
-        let parsed = shared::parse(contents);
+
+    fn test_part1() {
+        let sample1: &str = include_str!("../inputs/sample.txt");
+        let sample2: &str = include_str!("../inputs/sample2.txt");
+        let sample3: &str = include_str!("../inputs/sample3.txt");
+        test_part1_with_sample(sample1, 10);
+        test_part1_with_sample(sample2, 19);
+        test_part1_with_sample(sample3, 226);
+    }
+    fn test_part1_with_sample(sample: &str, expected: u64) {
+        let parsed = shared::parse(sample);
         println!("{:?}", parsed);
-        let part1 = soln1::Soln1::part1(contents);
+        let part1 = soln1::Soln1::part1(sample);
         println!("{}", part1);
-        // assert_eq!(part1, 1656);
-        // let part2 = soln1::Soln1::part2(contents);
-        // println!("{:?}", part2);
-        // assert_eq!(part2, Some(195));
+        assert_eq!(part1, expected);
     }
 }
--- day12/src/soln1.rs
+++ day12/src/soln1.rs
@@ -1,8 +1,10 @@
+// use crate::shared;
 use crate::shared::AdjList;
+// use shared;
 use std::collections::{HashSet, VecDeque};
 
-#[path = "shared.rs"]
-mod shared;
+// #[path = "shared.rs"]
+// mod shared;
 
 pub struct Soln1 {}
 impl Soln1 {
@@ -28,14 +30,38 @@ impl Soln1 {
         }
         npaths
     }
+
+    ///Naive backtracking version
     pub fn part1(input: &str) -> u64 {
-        let adj_list = shared::parse(input);
-        let end_adjs = adj_list.nbrs.get("end").unwrap();
+        let adj_list = crate::shared::parse(input);
         let mut npaths = 0u64;
-        for end_adj in end_adjs {
-            npaths += Self::npaths_from(*end_adj, &adj_list);
-        }
+        Self::backtrack(&adj_list, &mut vec!["start".to_string()], &mut npaths, &HashSet::new());
         npaths
+    }
+
+    pub fn backtrack(
+        adj_list: &AdjList,
+        mut current_path: &mut Vec<String>,
+        npaths: &mut u64,
+        visited: &HashSet<String>,
+    ) {
+        let tail = current_path.last().unwrap().clone();
+        if tail == "end" {
+            // println!("discovered path {:?}", current_path);
+            *npaths += 1;
+        }
+        let mut new_visited = visited.clone();
+        if (tail.to_uppercase() != *tail) {
+            new_visited.insert(tail.clone());
+        }
+        for nbr in adj_list.nbrs.get(&tail).unwrap() {
+            //try path current -> nbr
+            if (!visited.contains(nbr)) {
+                current_path.push(nbr.clone());
+                Self::backtrack(adj_list, &mut current_path.clone(), npaths, &new_visited);
+                current_path.pop();
+            }
+        }
     }
 
     pub fn part2(input: &str) -> Option<usize> {
