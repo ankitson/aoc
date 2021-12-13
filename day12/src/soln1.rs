2f9061a2ffd5d80410aa668b46c6602a5bcfebc6
Ankit Soni <ankitsoni9@gmail.com>
2021-12-13 11:15:02 +0000

    puzzle: see day12 src in this commit

--- day12/src/soln1.rs
+++ day12/src/soln1.rs
@@ -6,60 +6,36 @@ mod shared;
 
 pub struct Soln1 {}
 impl Soln1 {
-    /*
-        start
-       /  |  \
-      a . C   b
-      npaths(start,end) = npaths(a,end, visit=any) + npaths(b,end, visit=any + npaths(C,end,visit=any-C)
-
-      BFS, but when visiting a capital letter, remove it from the visited set.?
-      increment npaths when end is reached
-
-      start
-       /\
-    c-A--b-d
-      \  /
-       end
-    */
-
-    /*
-     * Another approach:
-     * 
-     * np(p,q) = number of paths(p,q) = sum ( number of paths (p, vi) * number of paths (vi, q) )
-     *  
-     * we want np(start,end) = sum (np(start,s1),np(start,s2),..,np(start,si)) si = nbr of start
-     * 
-     * calculate np for all pairs?
-     * 
-     * visit start
-     *   visit A
-     *      np(start,A) = 1
-     *      visit c
-     *          np(A,c) = 1
-     *      np(A,A) = 1
-     *      visit b
-     *          np(A,b) = 1
-     *          visit d
-     *              np(b,d) = 1
-     * 
-     *          visit end
-     *              np(b,end) = 1
-     *          
-     *          visit A
-     *              np(b,A) = 2
-     *      visit end
-     *          np(A,end)      
-     *          
-     *          
-     * 
-     * 
-     * 
-     * 
-     */
-
-
+    pub fn npaths_from(start: String, adj_list: &AdjList) -> u64 {
+        let mut to_visit: VecDeque<(String, String)> = VecDeque::from([(start, "none".to_string())]);
+        let mut visited_edges: HashSet<(String, String)> = HashSet::new();
+        let mut npaths = 0u64;
+        while !to_visit.is_empty() {
+            let (visit, from) = to_visit.pop_back().unwrap();
+            if visit == "end" {
+                npaths += 1;
+            }
+            let empty: Vec<String> = vec![];
+            for nbr in adj_list.nbrs.get(&visit).unwrap_or(&empty) {
+                if !visited_edges.contains(&(nbr.to_string(), visit.to_string())) {
+                    to_visit.push_front((nbr.to_string(), visit.to_string()));
+                }
+            }
+            visited_edges.insert((visit.to_string(), from.to_string()));
+            if (visit.to_lowercase() == visit && from.to_lowercase() == from) {
+                visited_edges.insert((from.to_string(), visit.to_string()));
+            }
+        }
+        npaths
+    }
     pub fn part1(input: &str) -> u64 {
-        todo!()
+        let adj_list = shared::parse(input);
+        let end_adjs = adj_list.nbrs.get("end").unwrap();
+        let mut npaths = 0u64;
+        for end_adj in end_adjs {
+            npaths += Self::npaths_from(*end_adj, &adj_list);
+        }
+        npaths
     }
 
     pub fn part2(input: &str) -> Option<usize> {
