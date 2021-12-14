332d9cccc38530741d06af38361df6f54a149869
Ankit Soni <ankitsoni9@gmail.com>
2021-12-14 16:28:56 +0000

    day 12 cleanup

--- day12/benches/corebench.rs
+++ day12/benches/corebench.rs
@@ -1,14 +1,13 @@
-#![feature(array_zip)]
 use criterion::{criterion_group, criterion_main, Criterion};
-include!("../src/soln1.rs");
+use day12::soln1::Soln1;
 
 pub fn benchmarks(c: &mut Criterion) {
-    // let contents: &str = include_str!("../inputs/day11.txt");
+    let contents: &str = include_str!("../inputs/day12.txt");
 
-    // let mut group = c.benchmark_group("day11");
-    // group.bench_function("part1", |b| b.iter(|| Soln1::part1(contents)));
-    // group.bench_function("part2", |b| b.iter(|| Soln1::part2(contents)));
-    // group.finish()
+    let mut group = c.benchmark_group("day12");
+    group.bench_function("part1", |b| b.iter(|| Soln1::part1(contents)));
+    group.bench_function("part2", |b| b.iter(|| Soln1::part2(contents)));
+    group.finish()
 }
 
 criterion_group!(benches, benchmarks);
