7d5724866b62c1091c3836af0c90d99cd1117930
Ankit Soni <ankitsoni9@gmail.com>
2021-12-14 08:40:08 +0000

    cleanup benches

--- day14/benches/corebench.rs
+++ day14/benches/corebench.rs
@@ -1,38 +1,20 @@
-// use super::shared;
-
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use day14::shared::parse;
 use day14::soln1::Soln1;
-
-// include!("../src/shared.rs");
-// include!("../src/soln1.rs");
 
 pub fn benchmarks(c: &mut Criterion) {
     let contents: &str = include_str!("../inputs/day14.txt");
 
     let (poly, rules) = parse(contents);
-    // let final_poly = Soln1::apply_n(poly, rules, 10);
-    // Self::score(&final_poly)
-
     let mut group = c.benchmark_group("day14");
-    group.bench_function("total (5)", |b| {
-        b.iter(|| {
-            let final_poly = Soln1::apply_n(poly, rules.to_owned(), 5);
-            Soln1::score(&final_poly)
-        })
-    });
-    group.bench_function("total (8)", |b| {
-        b.iter(|| {
-            let final_poly = Soln1::apply_n(poly, rules.to_owned(), 8);
-            Soln1::score(&final_poly)
-        })
-    });
-    group.bench_function("total (12)", |b| {
-        b.iter(|| {
-            let final_poly = Soln1::apply_n(poly, rules.to_owned(), 12);
-            Soln1::score(&final_poly)
-        })
-    });
+    for iters in 5..=12 {
+        group.bench_with_input(BenchmarkId::new("total", iters), &iters, |b, &i| {
+            b.iter(|| {
+                let final_poly = Soln1::apply_n(poly, rules.to_owned(), i);
+                Soln1::score(&final_poly)
+            })
+        });
+    }
     group.finish()
 }
 
--- day14/perf.md
+++ day14/perf.md
@@ -1,7 +1,34 @@
 Naive (f8abcb8)
 ------
+
 ```
-day14/total (5)         time:   [142.80 us 144.13 us 145.67 us]                           
-day14/total (8)         time:   [2.8293 ms 2.8371 ms 2.8423 ms]                            
-day14/total (12)        time:   [503.46 ms 514.25 ms 528.47 ms]                           
+day14/total/5           time:   [142.13 us 142.60 us 143.42 us]                         
+day14/total/6           time:   [345.76 us 347.66 us 352.11 us]                         
+day14/total/7           time:   [924.57 us 926.80 us 930.17 us]                          
+day14/total/8           time:   [2.8036 ms 2.8127 ms 2.8275 ms]                          
+day14/total/9           time:   [9.3489 ms 9.3599 ms 9.3789 ms]                         
+day14/total/10          time:   [34.089 ms 34.302 ms 34.503 ms]                          
+day14/total/11          time:   [129.18 ms 129.58 ms 130.22 ms]                         
+day14/total/12          time:   [505.42 ms 507.37 ms 509.25 ms] 
 ```
+
+exponential
+
+Let S = length(poly)
+    R = length(rules)
+    N = num iters
+
+for each pair of characters - O(S) pairs
+  for each rule - O(R) rules
+    get pair - O(1) assuming the iterator has a fast skip
+    add char - O(1) amortized append to array
+apply = O(S*R) time
+
+assuming string length grows exponentially,
+
+apply_nth = O(2^n * S * R) time
+
+
+
+
+
