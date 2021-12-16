a0db921ae84dada5e852623bd031fc6718472701
Ankit Soni <ankitsoni9@gmail.com>
2021-12-16 09:58:34 +0000

    day 16 scaffold

---
+++ day16/src/main.rs
@@ -0,0 +1,32 @@
+mod shared;
+mod soln1;
+
+pub fn main() {
+    println!("Hello Day 16!");
+    let contents: &str = include_str!("../inputs/day16.txt");
+    // let part1 = soln1::Soln1::part1(contents);
+    // println!("Part 1 =  {:?}", part1);
+    // let part2 = soln1::Soln1::part2(contents);
+    // println!("Part 2 =  {:?}", part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use day16::soln1;
+
+    #[test]
+    fn test_part1() {
+        let sample: &str = include_str!("../inputs/sample.txt");
+        // let part1 = soln1::Soln1::part1_slow(sample);
+        // println!("Part 1 (slow) = {:?}", part1);
+        // assert_eq!(part1, 40);
+    }
+
+    #[test]
+    fn test_part2() {
+        let sample: &str = include_str!("../inputs/sample.txt");
+        // let part2 = soln1::Soln1::part2(sample);
+        // println!("Part 2 (djikstra) = {:?}", part2);
+        // assert_eq!(part2, 315);
+    }
+}
---
+++ day16/src/soln1.rs
@@ -0,0 +1,6 @@
+// use crate::shared::parse;
+// use itertools::Itertools;
+
+pub struct Soln1 {}
+
+impl Soln1 {}
