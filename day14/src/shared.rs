e32e2850c36f00dc7bbbb8899a2ab60cee7329f9
Ankit Soni <ankitsoni9@gmail.com>
2021-12-16 08:57:38 +0000

    day14 refactor

--- day14/src/shared.rs
+++ day14/src/shared.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Rule<'a> {
     pub from: &'a str,
@@ -27,4 +29,24 @@ pub fn parse(input: &str) -> (&str, Vec<Rule>) {
     }
 
     (poly, rules)
+}
+
+type RuleMap = HashMap<(char, char), char>;
+
+pub fn parse2(input: &str) -> (&str, RuleMap) {
+    let mut lines = input.lines();
+
+    let poly = lines.next().unwrap();
+    lines.next();
+
+    let mut rule_map: RuleMap = HashMap::new();
+    for line in lines {
+        let parts = line.split_once(" -> ").unwrap();
+        let (from, to) = (parts.0, parts.1);
+        let mut from_chars = from.chars();
+        let c1 = from_chars.next().unwrap();
+        let c2 = from_chars.next().unwrap();
+        rule_map.insert((c1, c2), to.chars().next().unwrap());
+    }
+    (poly, rule_map)
 }
