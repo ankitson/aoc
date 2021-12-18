08e177fbc1762838b1a89d4e9879ef756e25a38d
Ankit Soni <ankitsoni9@gmail.com>
2021-12-18 06:30:50 +0000

    wip

--- day18/src/shared.rs
+++ day18/src/shared.rs
@@ -1,15 +1,108 @@
 use itertools::Itertools;
+use std::{
+    collections::{BinaryHeap, VecDeque},
+    ops::Index,
+};
 
-pub fn parse(input: &str) -> isize {
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum PEH {
+    Leaf(usize),
+    Node(Box<PEH>, Box<PEH>),
+}
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct PEHQ {
+    peh: PEH,
+    priority: usize,
+}
+
+impl Ord for PEHQ {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.priority.cmp(&other.priority)
+    }
+}
+
+impl PartialOrd for PEHQ {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// Desired structure..
+///
+/// [[6,[5,[4,[3,2]]]], 1]
+/// becomes (num,depth) pairs
+/// [(6,1),(5,2),(4,3),(3,4),(2,4),(1,0)]
+///
+/// now explode: find depth 4 pair and replace
+///
+
+pub fn parse(input: &str) -> PEH {
+    let chars = input.chars().collect_vec();
+    let mut vec: Vec<(usize, usize)> = Vec::new();
+    let node = parse_rec(&chars, 0, &mut vec);
+    println!("parsed: {:?}", node);
     todo!()
+}
+
+pub fn parse_rec(chars: &[char], depth: usize, vec: &mut Vec<(usize, usize)>) -> () {
+    if chars.is_empty() {
+        return;
+    }
+
+    if chars[0].is_numeric() {
+        vec.push((chars[0].to_digit(10).unwrap().try_into().unwrap(), depth));
+        parse_rec(&chars[1..], depth, vec);
+    }
+
+    if chars[0] == '[' {
+        parse_rec(&chars[1..], depth + 1, vec);
+    } else if chars[0] == ']' {
+        parse_rec(&chars[1..], depth - 1, vec);
+    } else {
+        parse_rec(&chars[1..], depth, vec)
+    }
+
+    // let mut depth = 0;
+    // let mut q: BinaryHeap<PEHQ> = BinaryHeap::new();
+    // let mut cur: Vec<usize> = Vec::new();
+    // let mut i = 0;
+    // while i < chars. {
+    //     let ch = chars.next().unwrap();
+    //     if ch == '[' {
+    //         depth += 1;
+    //         continue;
+    //     } else if ch == ']' {
+    //         if cur.len() == 2 {
+    //             let node = PEH { }
+    //         }
+    //         depth -= 1;
+    //         // let
+    //     } else if ch == ',' {
+    //         let next = parse(input)
+    //         continue;
+    //     } else {
+    //         let digit = ch.to_digit(10).unwrap();
+
+    //     }
+    // }
+    // todo!(
 }
 
 #[cfg(test)]
 mod tests {
-    use super::parse;
+    use itertools::Itertools;
+
+    //notes
+    // i spent too long thinking of how to parse before thinking about what structure i want to parse to.
+    // a tree structure is natural, but not super convenient for this problem
+    use super::{parse, parse_rec};
     #[test]
     fn test_parse() {
-        let sample: &str = include_str!("../inputs/sample.txt");
+        let sample: &str = "[[6,[5,[4,[3,2]]]],1]";
+        let mut vec = Vec::new();
+        parse_rec(&sample.chars().collect_vec(), 0, &mut vec);
+        assert_eq!(vec, vec![(6, 2), (5, 3), (4, 4), (3, 5), (2, 5), (1, 1)])
+
         // assert_eq!(parse(sample), ((20, 30), (-10, -5)))
     }
 }
