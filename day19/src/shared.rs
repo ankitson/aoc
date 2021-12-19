8ba7bdf49eed6ced1b646ad671c76dcc05af3123
Ankit Soni <ankitsoni9@gmail.com>
2021-12-19 11:49:20 +0000

    day 19 parse

--- day19/src/shared.rs
+++ day19/src/shared.rs
@@ -1,10 +1,64 @@
-use itertools::Itertools;
+use std::{collections::HashMap, iter::Scan};
 
-pub fn parse(input: &str) -> &str {
-    todo!()
+use itertools::Itertools;
+use regex::Regex;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Coord(isize, isize, isize);
+pub type ScanCoords = HashMap<usize, Vec<Coord>>;
+
+pub fn parse(input: &str) -> ScanCoords {
+    let scanner_num_regex = Regex::new(r"--- scanner (\d+) ---").unwrap();
+    let mut scan_coords: ScanCoords = HashMap::new();
+    let mut coords = Vec::new();
+    let mut current_scanner = None;
+    for line in input.lines() {
+        if line.is_empty() || line.chars().all(|c| c.is_ascii_whitespace()) {
+            continue;
+        }
+        if let Some(matches) = scanner_num_regex.captures(line) {
+            if current_scanner.is_some() {
+                scan_coords.insert(current_scanner.unwrap(), coords.to_vec());
+                coords.clear();
+            }
+            current_scanner = Some(matches.get(1).unwrap().as_str().parse().unwrap());
+        } else if let Some((x, y, z)) = line
+            .splitn(3, ',')
+            .map(|s| {
+                println!("parsing {} as int", s);
+                s.parse::<isize>().unwrap()
+            })
+            .collect_tuple()
+        {
+            coords.push(Coord(x, y, z));
+        }
+    }
+    if !coords.is_empty() {
+        scan_coords.insert(current_scanner.unwrap(), coords.to_vec());
+        coords.clear();
+    }
+    scan_coords
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::shared::Coord;
+
     use super::parse;
+
+    #[test]
+    fn test_parse_sample() {
+        let sample = include_str!("../inputs/sample.txt");
+        let coord_map = parse(sample);
+        assert_eq!(coord_map.len(), 5);
+        assert_eq!(coord_map.get(&0).unwrap()[0], Coord(404, -588, -901));
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let sample = include_str!("../inputs/day19.txt");
+        let coord_map = parse(sample);
+        assert_eq!(coord_map.len(), 35);
+        assert_eq!(*coord_map.get(&34).unwrap().last().unwrap(), Coord(-660, 739, 490));
+    }
 }
--- day19/src/soln1.rs
+++ day19/src/soln1.rs
@@ -1,4 +1,33 @@
 use crate::shared::parse;
+
+///
+/// 1D case:
+/// scanner at 0, beacon at 1
+/// possible reported positions: [1,-1]
+///
+/// 2D case:
+/// scanner at (0,0), beacon at (1,2)
+/// rotate around Z axis
+/// 0   : x->x, y->y
+/// 90  : x->y, y->-x
+/// 180 : x->-x, y->-y
+/// 270 : x->y, y->x
+/// map positive x to one of 4 directions
+/// then map positive y to one of 2 directions? (but 3rd is ruled out because it will be = -x)
+/// so 4*2 = 8 total orientations?
+///
+/// rotation matrix:
+/// [1 0] * [1 0]  = [1 0]
+///         [0 1]
+///
+/// 3D case:
+///
+/// face +- X,Y or Z -> 12 choices
+/// consider
+///
+///
+/// rotate
+/// ]
 pub fn part1(input: &str) -> usize {
     todo!()
 }
