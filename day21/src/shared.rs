0f8a575a8c5e8659a211099ac82fe0cfc80f1817
Ankit Soni <ankitsoni9@gmail.com>
2021-12-21 03:29:32 -0800

    [day21] part1 done

--- day21/src/shared.rs
+++ day21/src/shared.rs
@@ -1,7 +1,15 @@
 use itertools::Itertools;
+use regex::Regex;
 
-pub fn parse(input: &str) -> String {
-    todo!();
+pub fn parse(input: &str) -> Vec<usize> {
+    let mut lines = input.lines().filter(|l| !l.is_empty());
+    let mut starts: Vec<usize> = Vec::new();
+    let re = Regex::new(r"(\d+)($|\n)").unwrap();
+    for cap in re.captures_iter(input) {
+        let parsed = &cap[1].parse::<usize>().unwrap();
+        starts.push(*parsed);
+    }
+    starts
 }
 
 #[cfg(test)]
@@ -9,5 +17,8 @@ mod tests {
     use super::parse;
 
     #[test]
-    fn test_parse() {}
+    fn test_parse() {
+        assert_eq!(parse(include_str!("../inputs/sample.txt")), vec![4, 8]);
+        assert_eq!(parse(include_str!("../inputs/day21.txt")), vec![8, 1]);
+    }
 }
--- day21/src/soln1.rs
+++ day21/src/soln1.rs
@@ -4,8 +4,33 @@ use std::iter::repeat;
 
 pub struct Soln1 {}
 impl Soln1 {
+    fn next_position(mut position: usize, die: usize, circle_size: usize) -> usize {
+        let incr = die + die + 1 + die + 2;
+        position -= 1;
+        position += incr;
+        position %= circle_size;
+        position += 1;
+        position
+    }
     pub fn part1(input: &str) -> usize {
-        todo!()
+        let mut poses = parse(input);
+        let num_players = poses.len();
+        let mut scores = vec![0; num_players];
+        let mut player = 0;
+        let mut die = 1;
+        let mut nturns = 0;
+
+        loop {
+            poses[player] = Self::next_position(poses[player], die, 10);
+            scores[player] += poses[player];
+            nturns += 1;
+            die += 3;
+            if scores[player] >= 1000 {
+                let losing_score = scores[1 - player];
+                return losing_score * nturns * 3;
+            }
+            player = (player + 1) % num_players;
+        }
     }
 
     pub fn part2(input: &str) -> usize {
@@ -22,5 +47,6 @@ mod tests {
     #[test]
     fn test_part1() {
         let sample: &str = include_str!("../inputs/sample.txt");
+        assert_eq!(Soln1::part1(sample), 739785);
     }
 }
