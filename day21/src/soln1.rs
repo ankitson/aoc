9481837fcd953dd775436b46907c04a118bef5cd
Ankit Soni <ankitsoni9@gmail.com>
2021-12-21 13:39:40 -0800

    naive version

--- day21/src/soln1.rs
+++ day21/src/soln1.rs
@@ -1,17 +1,8 @@
 use crate::shared::parse;
-use itertools::Itertools;
-use std::iter::repeat;
-
+use itertools::{iproduct, izip, Itertools};
+use rayon::prelude::*;
 pub struct Soln1 {}
 impl Soln1 {
-    fn next_position(mut position: usize, die: usize, circle_size: usize) -> usize {
-        let incr = die + die + 1 + die + 2;
-        position -= 1;
-        position += incr;
-        position %= circle_size;
-        position += 1;
-        position
-    }
     pub fn part1(input: &str) -> usize {
         let mut poses = parse(input);
         let num_players = poses.len();
@@ -21,7 +12,7 @@ impl Soln1 {
 
         loop {
             let player = nturns % num_players;
-            poses[player] = Self::next_position(poses[player], die, 10);
+            poses[player] = Self::incr_pos(poses[player], 3 * die + 3);
             scores[player] += poses[player];
             nturns += 1;
             die += 3;
@@ -41,7 +32,14 @@ impl Soln1 {
         posi.try_into().unwrap()
     }
 
-    fn step_n(starts: Vec<usize>, rolls: usize, scores: &mut [usize], wins1: &mut u64, wins2: &mut u64) {
+    fn step_n(
+        starts: Vec<usize>,
+        rolls: usize,
+        mut roll_sub: usize,
+        scores: &mut [usize],
+        wins1: &mut u64,
+        wins2: &mut u64,
+    ) {
         let turn = (42 - rolls) % 2; //at 21 rolls remaining, its p1s turn.
 
         if scores[1 - turn] > 21 {
@@ -55,13 +53,102 @@ impl Soln1 {
 
         let prefix: String = ".".repeat(42 - rolls);
         println!("{} {}, {}", prefix, scores[0], scores[1]);
-        for roll in 1..4 {
-            let mut new_starts = starts.clone();
-            new_starts[turn] = Self::incr_pos(starts[turn], roll);
-            let mut new_scores = &mut scores[..]; //scores.clone();
-            new_scores[turn] += new_starts[turn];
+        let mut next_rolls = rolls;
+        if roll_sub == 1 {
+            next_rolls -= 1;
+            roll_sub = 3;
+        };
+        for i in 0..roll_sub {
+            for roll in 1..=3 {
+                let mut new_starts = starts.clone();
+                new_starts[turn] = Self::incr_pos(starts[turn], roll);
+                let mut new_scores = &mut scores[..]; //scores.clone();
+                new_scores[turn] += new_starts[turn];
 
-            Self::step_n(new_starts, rolls - 1, new_scores, wins1, wins2)
+                Self::step_n(new_starts, next_rolls, roll_sub - 1, new_scores, wins1, wins2)
+            }
+        }
+    }
+    
+    pub fn recurse(
+        p1pos: usize,
+        p2pos: usize,
+        p1score: usize,
+        p2score: usize,
+        p1wins: &mut u64,
+        p2wins: &mut u64,
+        turn: (usize, usize),
+        depth: usize,
+        longest: &mut usize,
+        mut last_rolls: Vec<usize>
+    ) {
+        let (pturn, num_rolls_left) = turn;
+        if depth > *longest {
+            *longest = depth;
+        }
+        if p1score >= 21 {
+            *p1wins += 1;
+            return;
+        } else if p2score >= 21 {
+            *p2wins += 1;
+            return;
+        }
+        if p1score >= 21 && p2score >= 21 {
+            panic!("illegal state??")
+        }
+
+        // (1..=3).par_map(|roll| 
+        for roll in 1..=3 {
+            if pturn == 0 {
+                //p1s turn
+                let nextpos = if num_rolls_left == 1 { Self::incr_pos(Self::incr_pos(Self::incr_pos(p1pos, last_rolls[0]),last_rolls[1]),roll) } else { p1pos };
+                let nextscore = if num_rolls_left == 1 { p1score + nextpos } else { p1score };
+                let next_turn = if num_rolls_left == 1 { 1 - pturn } else { pturn };
+                let next_rolls = if num_rolls_left == 1 { 3 } else { num_rolls_left - 1 };
+                let next_last_rolls = if num_rolls_left == 1 { vec![] } else { let mut nr = last_rolls.clone(); nr.push(roll); nr };
+                let prefix = ".".repeat(depth + 1);
+                // println!(
+                //     "{}P1 rolled {} . Score {} . Next turn {},{}",
+                //     prefix, roll, nextscore, next_turn, next_rolls
+                // );
+                Self::recurse(
+                    nextpos,
+                    p2pos,
+                    nextscore,
+                    p2score,
+                    p1wins,
+                    p2wins,
+                    (next_turn, next_rolls),
+                    depth + 1,
+                    longest,
+                    next_last_rolls
+                )
+            } else {
+                let nextpos = if num_rolls_left == 1 { Self::incr_pos(Self::incr_pos(Self::incr_pos(p1pos, last_rolls[0]),last_rolls[1]),roll) } else { p2pos };
+                let nextscore = if num_rolls_left == 1 { p1score + nextpos } else { p1score };
+                // let nextpos = Self::incr_pos(p2pos, roll);
+                let next_turn = if num_rolls_left == 1 { 1 - pturn } else { pturn };
+                let next_rolls = if num_rolls_left == 1 { 3 } else { num_rolls_left - 1 };
+                let next_last_rolls = if num_rolls_left == 1 { vec![] } else { let mut nr = last_rolls.clone(); nr.push(roll); nr };
+                let prefix = ".".repeat(depth + 1);
+                // println!(
+                //     "{}P2 rolled {} . Score {} . Next turn {},{}",
+                //     prefix, roll, nextscore, next_turn, next_rolls
+                // );
+
+                Self::recurse(
+                    p1pos,
+                    nextpos,
+                    p1score,
+                    nextscore,
+                    p1wins,
+                    p2wins,
+                    (next_turn, next_rolls),
+                    depth + 1,
+                    longest,
+                    next_last_rolls
+                )
+            }
         }
     }
 
@@ -69,8 +156,20 @@ impl Soln1 {
         let mut poses = parse(input);
         let mut wins1 = 0u64;
         let mut wins2 = 0u64;
-        Self::step_n(poses, 42, &mut [0; 2], &mut wins1, &mut wins2);
+        Self::recurse(
+            poses[0],
+            poses[1],
+            0,
+            0,
+            &mut wins1,
+            &mut wins2,
+            (0, 3),
+            0,
+            &mut 0,
+            vec![]
+        );
         (wins1, wins2)
+        //Self::step_n(poses, 42, 3, &mut [0; 2], &mut wins1, &mut wins2);
     }
 }
 
@@ -85,6 +184,6 @@ mod tests {
         let sample: &str = include_str!("../inputs/sample.txt");
         assert_eq!(Soln1::part1(sample), 739785);
 
-        println!("{:?}", Soln1::part2(sample));
+        println!("Part 2 (sample) = {:?}", Soln1::part2(sample));
     }
 }
