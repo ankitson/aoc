2c0c812bab799230c3b3d0fb8cf9c13cd0d73f11
Ankit Soni <ankitsoni9@gmail.com>
2021-12-21 20:21:50 -0800

    [day21] dumb broken

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -3,6 +3,8 @@
 #![allow(unused_parens)]
 #![feature(int_roundings)]
 
+use std::collections::HashMap;
+
 mod shared;
 mod soln1;
 
@@ -11,6 +13,19 @@ pub fn main() {
     let contents: &str = include_str!("../inputs/day21.txt");
     let part1 = soln1::Soln1::part1(contents);
     println!("Part 1 =  {:?}", part1);
-    let part2 = soln1::Soln1::part2(contents);
-    println!("Part 2 =  {:?}", part2);
+
+    let sample: &str = include_str!("../inputs/sample.txt");
+    let cache = HashMap::new();
+    let mut soln1 = soln1::Soln1::new(cache);
+    for target in 2..3 {
+        println!("Part 2 (sample) (target = {}) = {:?}", target, soln1.part2(sample, target));
+        soln1.cache.clear();
+    }
+
+    // println!("{}", soln1::Soln1::part22(contents));
+
+    // println!("Part 2 (sample) = {:?}", soln1::Soln1::part2(sample));
+
+    // let part2 = soln1::Soln1::part2(contents);
+    // println!("Part 2 =  {:?}", part2);
 }
--- day21/src/soln1.rs
+++ day21/src/soln1.rs
@@ -1,8 +1,17 @@
 use crate::shared::parse;
 use itertools::{iproduct, izip, Itertools};
 use rayon::prelude::*;
-pub struct Soln1 {}
+use std::{sync::atomic::{AtomicU64, Ordering}, collections::HashMap};
+
+pub struct Soln1 {
+    pub cache: HashMap<(usize,usize,usize,usize,(usize,usize),usize),(u64,u64)>
+}
 impl Soln1 {
+    pub fn new(cache: HashMap<(usize,usize,usize,usize,(usize,usize),usize),(u64,u64)>) -> Self { Self { cache } }
+
+    // pub fn default() -> Self {
+        // Soln1 { cache: &'static mut HashMap::new() }
+    // }
     pub fn part1(input: &str) -> usize {
         let mut poses = parse(input);
         let num_players = poses.len();
@@ -23,13 +32,15 @@ impl Soln1 {
         }
     }
 
-    fn incr_pos(mut pos: usize, roll: usize) -> usize {
-        let mut posi: isize = pos.try_into().unwrap();
-        let rolli: isize = roll.try_into().unwrap();
-        posi -= 1;
-        posi = (posi + rolli) % 10;
-        posi += 1;
-        posi.try_into().unwrap()
+    fn incr_pos(pos: usize, roll: usize) -> usize {
+        // let mut posi: isize = pos.try_into().unwrap();
+        // let rolli: isize = roll.try_into().unwrap();
+        // posi -= 1;
+        ((pos + roll + 10 - 1) % 10) + 1
+        // posi += 1;
+        // posi.try_into().unwrap()
+
+
     }
 
     fn step_n(
@@ -71,110 +82,124 @@ impl Soln1 {
     }
     
     pub fn recurse(
+        &mut self,
         p1pos: usize,
         p2pos: usize,
         p1score: usize,
         p2score: usize,
-        p1wins: &mut u64,
-        p2wins: &mut u64,
         turn: (usize, usize),
         depth: usize,
-        longest: &mut usize,
-        mut last_rolls: Vec<usize>
-    ) {
+        last_rolls: usize,
+        target: usize,
+    ) -> (u64, u64) {
         let (pturn, num_rolls_left) = turn;
-        if depth > *longest {
-            *longest = depth;
+        
+        println!("recurse: {} {} {} {} {:?} {}",p1pos,p2pos,p1score,p2score,turn,last_rolls);
+        //10*10*20*20*2 = 80000 unique kys
+        // if (self.cache.contains_key(&(p1pos,p2pos,p1score,p2score,turn,last_rolls))) {
+        //     let val = *self.cache.get(&(p1pos,p2pos,p1score,p2score,turn,last_rolls)).unwrap();
+        //     println!("cache hit for {:?} = {:?}", (p1pos,p2pos,p1score,p2score,turn,last_rolls), val);
+        //     return val;
+        // }
+
+        if p1score >= target {
+            return (1,0);
+        } else if p2score >= target {
+            return (0,1);
         }
-        if p1score >= 21 {
-            *p1wins += 1;
-            return;
-        } else if p2score >= 21 {
-            *p2wins += 1;
-            return;
-        }
-        if p1score >= 21 && p2score >= 21 {
+        if p1score >= target && p2score >= target {
             panic!("illegal state??")
         }
 
-        // (1..=3).par_map(|roll| 
-        for roll in 1..=3 {
+
+        let mut p1w = 0u64;
+        let mut p2w = 0u64;
+        (1..=3).for_each(|roll| {
             if pturn == 0 {
                 //p1s turn
-                let nextpos = if num_rolls_left == 1 { Self::incr_pos(Self::incr_pos(Self::incr_pos(p1pos, last_rolls[0]),last_rolls[1]),roll) } else { p1pos };
-                let nextscore = if num_rolls_left == 1 { p1score + nextpos } else { p1score };
-                let next_turn = if num_rolls_left == 1 { 1 - pturn } else { pturn };
-                let next_rolls = if num_rolls_left == 1 { 3 } else { num_rolls_left - 1 };
-                let next_last_rolls = if num_rolls_left == 1 { vec![] } else { let mut nr = last_rolls.clone(); nr.push(roll); nr };
+                let nextpos = if num_rolls_left == 0 { Self::incr_pos(p1pos, last_rolls+roll) } else { p1pos };
+                let nextscore = if num_rolls_left == 0 { p1score + nextpos } else { p1score };
+                // println("p1 scores {} after landing at {}")
+                let next_turn = if num_rolls_left == 0 { 1 - pturn } else { pturn };
+                let next_rolls = if num_rolls_left == 0 { 3 } else { num_rolls_left - 1 };
+                let next_last_rolls = if num_rolls_left == 0 { 0 } else { last_rolls + roll };
                 let prefix = ".".repeat(depth + 1);
                 // println!(
                 //     "{}P1 rolled {} . Score {} . Next turn {},{}",
                 //     prefix, roll, nextscore, next_turn, next_rolls
                 // );
-                Self::recurse(
+                
+                let (p1r, p2r) = self.recurse(
                     nextpos,
                     p2pos,
                     nextscore,
                     p2score,
-                    p1wins,
-                    p2wins,
                     (next_turn, next_rolls),
                     depth + 1,
-                    longest,
-                    next_last_rolls
-                )
+                    next_last_rolls,
+                    target
+                );
+                p1w += p1r;
+                p2w += p2r;
             } else {
-                let nextpos = if num_rolls_left == 1 { Self::incr_pos(Self::incr_pos(Self::incr_pos(p1pos, last_rolls[0]),last_rolls[1]),roll) } else { p2pos };
-                let nextscore = if num_rolls_left == 1 { p1score + nextpos } else { p1score };
+                let nextpos = if num_rolls_left == 0{ Self::incr_pos(p2pos, last_rolls+roll) } else { p2pos };
+                let nextscore = if num_rolls_left == 0 { p1score + nextpos } else { p1score };
                 // let nextpos = Self::incr_pos(p2pos, roll);
-                let next_turn = if num_rolls_left == 1 { 1 - pturn } else { pturn };
-                let next_rolls = if num_rolls_left == 1 { 3 } else { num_rolls_left - 1 };
-                let next_last_rolls = if num_rolls_left == 1 { vec![] } else { let mut nr = last_rolls.clone(); nr.push(roll); nr };
+                let next_turn = if num_rolls_left == 0 { 1 - pturn } else { pturn };
+                let next_rolls = if num_rolls_left == 0 { 3 } else { num_rolls_left - 1 };
+                let next_last_rolls = if num_rolls_left == 0 { 0 } else { last_rolls + roll };
                 let prefix = ".".repeat(depth + 1);
                 // println!(
                 //     "{}P2 rolled {} . Score {} . Next turn {},{}",
                 //     prefix, roll, nextscore, next_turn, next_rolls
                 // );
 
-                Self::recurse(
+                let (p1r,p2r) = self.recurse(
                     p1pos,
                     nextpos,
                     p1score,
                     nextscore,
-                    p1wins,
-                    p2wins,
                     (next_turn, next_rolls),
                     depth + 1,
-                    longest,
-                    next_last_rolls
-                )
+                    next_last_rolls,
+                    target
+                );
+                p1w += p1r;
+                p2w += p2r;
+                
             }
-        }
+        });
+        self.cache.insert((p1pos,p2pos,p1score,p2score,turn,last_rolls), (p1w,p2w));
+        (p1w, p2w)
     }
 
-    pub fn part2(input: &str) -> (u64, u64) {
+    pub fn part2(&mut self, input: &str, target: usize) -> (u64, u64) {
         let mut poses = parse(input);
-        let mut wins1 = 0u64;
-        let mut wins2 = 0u64;
-        Self::recurse(
+        let wins1 = AtomicU64::new(0);
+        let wins2 = AtomicU64::new(0);
+        // let mut wins1 = 0u64;
+        // let mut wins2 = 0u64;
+        // let mut memo = HashMap::new();
+        let (a,b) = self.recurse(
             poses[0],
             poses[1],
             0,
             0,
-            &mut wins1,
-            &mut wins2,
             (0, 3),
             0,
-            &mut 0,
-            vec![]
+            0,
+            target
         );
-        (wins1, wins2)
+        (a, b)
         //Self::step_n(poses, 42, 3, &mut [0; 2], &mut wins1, &mut wins2);
     }
+
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use crate::shared::parse;
 
     use super::Soln1;
@@ -184,6 +209,10 @@ mod tests {
         let sample: &str = include_str!("../inputs/sample.txt");
         assert_eq!(Soln1::part1(sample), 739785);
 
-        println!("Part 2 (sample) = {:?}", Soln1::part2(sample));
+        let cache = HashMap::new();
+        let mut soln1 = Soln1::new(cache);
+        println!("Part 2 (sample) = {:?}", soln1.part2(sample));
+
+        // println!("{}", Soln1::part22(sample));
     }
 }
