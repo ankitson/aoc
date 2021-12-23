dfc761eab84e11af9c3b1f67ca625961451b59cb
Ankit Soni <ankitsoni9@gmail.com>
2021-12-23 03:49:06 -0800

    [day23] scaffold

---
+++ day23/src/soln1.rs
@@ -0,0 +1,25 @@
+use crate::shared::parse;
+use itertools::Itertools;
+use std::iter::repeat;
+
+pub struct Soln1 {}
+impl Soln1 {
+    pub fn part1(input: &str) -> usize {
+        todo!()
+    }
+
+    pub fn part2(input: &str) -> usize {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Soln1;
+    use crate::shared::parse;
+
+    #[test]
+    fn test_part1() {
+        let sample: &str = include_str!("../inputs/sample.txt");
+    }
+}
