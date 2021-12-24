8d640041993ccd35cdc21c2657ff0248b179367d
Ankit Soni <ankitsoni9@gmail.com>
2021-12-24 01:38:47 -0800

    [day23] add benches

--- day23/benches/corebench.rs
+++ day23/benches/corebench.rs
@@ -1,12 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-// use day23::shared;
+use day23::shared;
 use day23::soln1;
 
 pub fn part1(c: &mut Criterion) {
     let contents: &str = include_str!("../inputs/day23.txt");
 
     let mut group = c.benchmark_group("day23.part1.realinput");
-    // group.bench_function("part1", |b| b.iter(|| soln1::Soln1::part1(black_box(contents))));
+    group.bench_function("part1", |b| b.iter(|| soln1::Soln1::<3>::part1_2(black_box(contents))));
     group.finish();
 }
 
@@ -14,7 +14,7 @@ pub fn part2(c: &mut Criterion) {
     let contents: &str = include_str!("../inputs/day23.txt");
 
     let mut group = c.benchmark_group("day23.part2.realinput");
-    // group.bench_function("part2", |b| b.iter(|| soln1::Soln1::part2(black_box(contents))));
+    group.bench_function("part2", |b| b.iter(|| soln1::Soln1::<5>::part2(black_box(contents))));
     group.finish();
 }
 
@@ -22,7 +22,7 @@ pub fn parse(c: &mut Criterion) {
     let contents: &str = include_str!("../inputs/day23.txt");
 
     let mut group = c.benchmark_group("day23.parse.realinput");
-    // group.bench_function("parse", |b| b.iter(|| shared::parse(black_box(contents))));
+    group.bench_function("parse", |b| b.iter(|| shared::parse::<3>(black_box(contents))));
     group.finish();
 }
 
