16382bdb4f3b418f74e3a241620d918e304982fa
Ankit Soni <ankitsoni9@gmail.com>
2021-12-25 05:34:57 -0800

    [day24] partial eval, borrow checker probs

--- day24/src/alu/adt.rs
+++ day24/src/alu/adt.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq, Eq, Clone)]
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 #[rustfmt::skip]
 pub enum Register { W, X, Y, Z, }
 
@@ -20,3 +22,44 @@ pub enum Instr {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Instrs(pub Vec<Instr>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ALU {
+    pub vals: HashMap<Register, isize>,
+}
+
+use Instr::{Add, Div, Eql, Inp, Mod, Mul};
+use Operand::{Literal, RegOp};
+use Register::{W, X, Y, Z};
+
+impl ALU {
+    pub fn default() -> Self {
+        let vals = HashMap::from_iter(vec![(W, 0), (X, 0), (Y, 0), (Z, 0)]);
+        ALU { vals }
+    }
+
+    fn get_operand_val(&self, operand: Operand) -> isize {
+        match operand {
+            RegOp(reg) => *self.vals.get(&reg).unwrap(),
+            Literal(i) => i,
+        }
+    }
+
+    pub fn eval_one(&mut self, instr: Instr, input: Option<isize>) -> () {
+        match instr {
+            Inp { dst } => {
+                self.vals.insert(dst, input.unwrap());
+            }
+            Add { dst, operand } => {
+                let d = self.vals.entry(dst).and_modify(|d| *d += self.get_operand_val(operand));
+                // d += self.get_operand_val(operand);
+                // let operand_val = self.vals.get_mut(&dst).map(|d| *d += self.get_operand_val(operand));
+            }
+            Mul { dst, operand } => todo!(),
+            Div { dst, operand } => todo!(),
+            Mod { dst, operand } => todo!(),
+            Eql { dst, operand } => todo!(),
+            // _ => panic!("not impl"),
+        }
+    }
+}
