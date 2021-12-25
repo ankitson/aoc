3a927c58a66ada0d74e36bbbed9ef8d5de252134
Ankit Soni <ankitsoni9@gmail.com>
2021-12-25 04:09:14 -0800

    [day24] finish parser

--- day24/src/alu.rs
+++ day24/src/alu.rs
@@ -65,45 +65,64 @@ pub enum Instr {
     Eql { dst: Register, operand: Operand },
 }
 
+#[rustfmt::skip]
 impl FromStr for Instr {
     type Err = ALUParseError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let parts = input.split_ascii_whitespace().collect_vec();
-        if parts.len() == 0 {
-            return Err(ALUParseError {
-                input: input.to_string(),
-                at: 0,
-                expect: "expected instr name".to_string(),
-                inner: None,
-            });
+        let parts = input.split_ascii_whitespace().collect_vec();        
+        if parts.is_empty() {
+            return Err(ALUParseError { input: input.to_string(), at: 0, expect: "expected instr name".to_string(), inner: None, });
         }
-        assert!(parts.len() >= 1 && parts.len() <= 3);
         match parts[0] {
+            "inp" => Register::from_str(parts[1]).map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
+                     .map(|reg| Instr::Inp { dst: reg }),
             "add" => Register::from_str(parts[1])
-                .map_err(|e| ALUParseError {
-                    input: input.to_string(),
-                    at: 1,
-                    expect: "expected register".to_string(),
-                    inner: Some(Ok(Box::new(e))),
-                })
+                .map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
                 .and_then(|dst| {
-                    let operand_maybe: Result<Operand, ALUParseError> = Operand::from_str(parts[2]);
-                    match operand_maybe {
+                    match Operand::from_str(parts[2]) {
                         Ok(operand) => Ok(Instr::Add { dst, operand }),
-                        Err(e) => Err(ALUParseError {
-                            input: input.to_string(),
-                            at: 2,
-                            expect: "expected operand".to_string(),
-                            inner: Some(Ok(Box::new(e))),
-                        }),
+                        Err(e) => Err(ALUParseError { input: input.to_string(), at: 2, expect: "expected operand".to_string(), inner: Some(Ok(Box::new(e))), }),
                     }
                 }),
+            "mul" => Register::from_str(parts[1])
+                .map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
+                .and_then(|dst| {
+                    match Operand::from_str(parts[2]) {
+                        Ok(operand) => Ok(Instr::Mul { dst, operand }),
+                        Err(e) => Err(ALUParseError { input: input.to_string(), at: 2, expect: "expected operand".to_string(), inner: Some(Ok(Box::new(e))), }),
+                    }
+                }),
+            "div" => Register::from_str(parts[1])
+                .map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
+                .and_then(|dst| {
+                    match Operand::from_str(parts[2]) {
+                        Ok(operand) => Ok(Instr::Div { dst, operand }),
+                        Err(e) => Err(ALUParseError { input: input.to_string(), at: 2, expect: "expected operand".to_string(), inner: Some(Ok(Box::new(e))), }),
+                    }
+                }),
+            "mod" => Register::from_str(parts[1])
+                .map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
+                .and_then(|dst| {
+                    match Operand::from_str(parts[2]) {
+                        Ok(operand) => Ok(Instr::Mod { dst, operand }),
+                        Err(e) => Err(ALUParseError { input: input.to_string(), at: 2, expect: "expected operand".to_string(), inner: Some(Ok(Box::new(e))), }),
+                    }
+                }),
+            "eql" => Register::from_str(parts[1])
+                .map_err(|e| ALUParseError { input: input.to_string(), at: 1, expect: "expected register".to_string(), inner: Some(Ok(Box::new(e))), })
+                .and_then(|dst| {
+                    match Operand::from_str(parts[2]) {
+                        Ok(operand) => Ok(Instr::Eql { dst, operand }),
+                        Err(e) => Err(ALUParseError { input: input.to_string(), at: 2, expect: "expected operand".to_string(), inner: Some(Ok(Box::new(e))), }),
+                    }
+                }),                             
             _ => panic!("unknown instruction: {}", parts[0]),
         }
     }
 }
 
+#[rustfmt::skip]
 #[cfg(test)]
 mod tests {
     use super::ALUParseError;
@@ -126,21 +145,25 @@ mod tests {
     #[test]
     fn parse_single() {
         assert_eq!(
-            parse_one("add w w", true),
-            Ok(Add {
-                dst: W,
-                operand: RegOp(W)
-            })
-        )
+            parse_one("inp x", true), Ok(Inp { dst: X })
+        );
+        assert_eq!(
+            parse_one("add w w", true), Ok(Add { dst: W, operand: RegOp(W) })
+        );
+        assert_eq!(
+            parse_one("add w 2", true), Ok(Add { dst: W, operand: Literal(2) })
+        );
+        assert_eq!(
+            parse_one("mul x w", true), Ok(Mul { dst: X, operand: RegOp(W) })
+        );
+        assert_eq!(
+            parse_one("div y z", true), Ok(Div { dst: Y, operand: RegOp(Z) })
+        );
+        assert_eq!(
+            parse_one("mod z 9", true), Ok(Mod { dst: Z, operand: Literal(9) })
+        );
+        assert_eq!(
+            parse_one("eql z w", true), Ok(Eql { dst: Z, operand: RegOp(W) })
+        );
     }
 }
-
-// struct ALU {}
-// impl ALU {
-//     pub fn parse_one(input: &str) -> Option<Instr> {
-//         let parts = input.split_ascii_whitespace().collect_vec();
-//         match parts[0] {
-//             "inp" => Instr(Input(parts[1])),
-//         }
-//     }
-// }
