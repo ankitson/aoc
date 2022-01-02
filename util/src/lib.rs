3392fd0ba8bcd87409da9e9a822f6dffc2acbd80
Ankit Soni <ankitsoni9@gmail.com>
2022-01-02 13:43:10 -0800

    update lib

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,4 +1,9 @@
 #![allow(unused_parens)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::iter::Copied;
 mod binary {
     fn fmt_bits(num: u8) -> String {
         let bytes = num.to_be_bytes();
@@ -34,50 +39,59 @@ pub fn abs_diff(a: usize, b: usize) -> usize {
         b - a
     }
 }
-pub mod aoc {
-    // use reqwest::{cookie, Client, ClientBuilder, Response, Url};
-    use cookie_store::{Cookie, CookieStore};
-    use std::{sync::Arc, time::Duration};
-    use ureq::Agent;
-    // use ureq::Cookie;
 
-    const SESSION: &str =
-        "53616c7465645f5f1a75ed6a2f81147d095d5166ad699642aa8a2a7b6e6a7c3a1f5bb2ec3ab81d562eba00d37189d57a";
-
-    //sync rust.. also tragic
-    fn fetch(day: usize) {
-        // let cks = CookieStore::default();
-        // let ck: Cookie = Cookie::parse(format!("session={}", SESSION), Url::parse("adv.com").unwrap()).unwrap();
-        // let ck = Cookie::parse(
-        //     format!(
-        //         "{}{}",
-        //         format!("session={}", SESSION),
-        //         ";Expires=\"Mon, 01 Dec 2031 07:11:25 GMT\"",
-        //     ),
-        //     ".adventofcode.com".parse::<Url>(), //&url(url_str),
-        // );
-        // let ck = Cookie::new("session", SESSION);
-        // // ck.insert(Cookie::new("session", SESSION), ".adventofcode.com");
-        // let agent: Agent = ureq::AgentBuilder::new()
-        //     .timeout_read(Duration::from_secs(5))
-        //     .timeout_write(Duration::from_secs(5))
-        //     .cookie_store(cks)
-        //     .build();
+mod conv {
+    #[derive(Debug)]
+    enum ConversionError {
+        Error,
     }
 
-    //async rust... a tragic tale
-    // pub async fn fetch_input(day: usize) -> Result<String, reqwest::Error> {
-    //     let url = format!("https://adventofcode.com/2021/day/{}/input", day)
-    //         .parse::<Url>()
-    //         .unwrap();
-    //     let cookie_jar = reqwest::cookie::Jar::default();
-    //     cookie_jar.add_cookie_str(&format!("session={}; Domain=.adventofcode.com", SESSION), &url);
-    //     let client = ClientBuilder::new()
-    //         .cookie_provider(Arc::new(cookie_jar))
-    //         .build()
-    //         .unwrap();
-    //     let response = client.get(url).send().await.unwrap();
-    //     let text = response.text().await;
-    //     text
-    // }
+    struct MyVec<T>(Vec<T>);
+
+    impl<T: TryInto<u32> + Copy> TryFrom<MyVec<T>> for u128 {
+        type Error = ConversionError;
+
+        fn try_from(vec: MyVec<T>) -> Result<Self, Self::Error> {
+            let mut chars = vec![];
+            for d in vec.0.iter() {
+                if let Ok(u) = (*d).try_into() {
+                    if let Some(ch) = char::from_digit(u, 10) {
+                        chars.push(ch)
+                    } else {
+                        return Err(ConversionError::Error);
+                    }
+                } else {
+                    return Err(ConversionError::Error);
+                }
+            }
+            match chars.iter().collect::<String>().parse::<u128>() {
+                Ok(n) => Ok(n),
+                Err(pi) => Err(ConversionError::Error),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::MyVec;
+
+        #[test]
+        fn test_conv() {
+            let digits: Vec<i32> = vec![1, 2, 3, 4];
+            let n: u128 = MyVec(digits).try_into().unwrap();
+            assert_eq!(n, 1234);
+
+            let digits: Vec<u32> = vec![1, 2, 3, 4];
+            let n: u128 = MyVec(digits).try_into().unwrap();
+            assert_eq!(n, 1234);
+
+            let digits: Vec<u64> = vec![1, 2, 3, 4];
+            let n: u128 = MyVec(digits).try_into().unwrap();
+            assert_eq!(n, 1234);
+
+            let digits: Vec<i64> = vec![1, 2, 3, 4];
+            let n: u128 = MyVec(digits).try_into().unwrap();
+            assert_eq!(n, 1234);
+        }
+    }
 }
