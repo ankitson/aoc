bb40abd431fa7fd4b9c72a8a96e97edb77649885
Ankit Soni <ankitsoni9@gmail.com>
2022-12-04 13:53:43 -0800

    2022 day 4 part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,14 +1,16 @@
 mod soln1;
 
 pub fn main() {
-    let contents: &str = include_str!("../inputs/day4.txt");
-    let (moves, boards) = soln1::Soln1::parse(contents);
-    let part1 = soln1::Soln1::part1(moves, boards);
-    let part1_out = soln1::Soln1::unparse(part1);
-    println!("Part 1 = {}", part1_out);
+    println!("Hello Day 3!");
+    let input: &str = include_str!("../inputs/sample4.txt");
+    let part1 = soln1::Soln1::part1(input);
+    println!("Part 1 / Sample = {:?}", part1); //2
 
-    let (moves, boards) = soln1::Soln1::parse(contents);
-    let part2 = soln1::Soln1::part2(moves, boards);
-    let part2_out = soln1::Soln1::unparse(part2);
-    println!("Part 2 = {}", part2_out);
+    let input: &str = include_str!("../inputs/day4.txt");
+    let part1 = soln1::Soln1::part1(input);
+    println!("Part 1 / Input 1 = {:?}", part1); //580
+
+    // let input: &str = include_str!("../inputs/day4.txt");
+    // let part2 = soln1::Soln1::part2(input);
+    // println!("Part 2 / Input 1 = {:?}", part2); //2602
 }
