a602daa882e04503ad8d92255ca59b03b2257a83
Ankit Soni <ankitsoni9@gmail.com>
2022-12-04 14:24:46 -0800

    Revert "buggy version of day 4 part 1"
    This reverts commit f9620f601ba8e9eab204f3f67fdbb7e37e8e108b.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,22 +7,10 @@ pub fn main() {
     println!("Part 1 / Sample = {:?}", part1); //2
 
     let input: &str = include_str!("../inputs/day4.txt");
-    let (overlaps1, count1) = soln1::Soln1::part1_wrong(input);
-    let (overlaps2, count2) = soln1::Soln1::part1(input);
+    let part1 = soln1::Soln1::part1(input);
+    println!("Part 1 / Input 1 = {:?}", part1); //580
 
-    //this reveals the bug: when both intervals have the same left endpoint,
-    //they will not be swapped. we assume r1 contains r2, but r2 may be the bigger one. 
-    println!("the wrong implementation missed {} overlaps", count2-count1);
-    for item in &overlaps2 {
-        if !(overlaps1.contains(item)) {
-            println!("{:?}", item);
-        }
-    }
-
-
-    // println!("Part 1 / Input 1 = {:?}", part1); //580
-
-    // let input: &str = include_str!("../inputs/day4.txt");
-    // let part2 = soln1::Soln1::part2(input);
-    // println!("Part 2 / Input 1 = {:?}", part2); //895
+    let input: &str = include_str!("../inputs/day4.txt");
+    let part2 = soln1::Soln1::part2(input);
+    println!("Part 2 / Input 1 = {:?}", part2); //895
 }
