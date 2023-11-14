901cc8814985436be81e52269eac6326f54d16ad
Ankit Soni <dev@ankitson.com>
2023-11-14 02:31:31 -0800

    2022-cleanup:day04:fix all warnings

--- 2022/rust/day04/src/soln1.rs
+++ 2022/rust/day04/src/soln1.rs
@@ -8,7 +8,7 @@ impl Soln1 {
     fn parse_input(input: &str) -> Vec<((i32, i32), (i32, i32))> {
         let mut result = vec![];
         for line in input.split('\n') {
-            if (line.is_empty()) { continue; }
+            if line.is_empty() { continue; }
             let mut numbers = line.split(',');
             let first = numbers.next().unwrap().split('-').map(|s| s.parse().unwrap()).collect_tuple().unwrap();
             let second = numbers.next().unwrap().split('-').map(|s| s.parse().unwrap()).collect_tuple().unwrap();
@@ -25,7 +25,7 @@ impl Soln1 {
                 mem::swap(&mut r1, &mut r2);
             }
             
-            if (r1.0 <= r2.0 && r1.1 >= r2.1) { // || (r2.0 <= r1.0 && r2.1 >= r1.1) {
+            if r1.0 <= r2.0 && r1.1 >= r2.1 { // || (r2.0 <= r1.0 && r2.1 >= r1.1) {
                 // println!("r2={:?} is contained in r1={:?}", r2, r1);
                 count += 1;
             }
@@ -40,7 +40,7 @@ impl Soln1 {
             if r1.0 > r2.0 { 
                 mem::swap(&mut r1, &mut r2);
             }
-            if (r2.0 <= r1.1) { count += 1}
+            if r2.0 <= r1.1 { count += 1}
         }
         count
     }
