ada487fba1023781fce21447006631c9067949f8
Ankit Soni <dev@ankitson.com>
2023-11-30 17:27:19 -0800

    2023: fix python benchmark scaffolding

--- 2023/python/bench.py
+++ 2023/python/bench.py
@@ -4,8 +4,7 @@ import pyperf
 import sys
 
 import bench_config
-import day07
-import day12
+import day01
 import util
 
 def str_result(result, tag):
@@ -32,6 +31,7 @@ def main():
   # Pyperf reuses sys.argv for its workers and i couldnt figure out how to inherit env
   # So, we read the day from another file and set it in the justfile before launch
   day = bench_config.day
+
   sample = open(util.sample_input(day),'r').read()
   input1 = open(util.real_input(day),'r').read()
   bench_file = open(util.bench_path(day),'w')
@@ -43,12 +43,11 @@ def main():
       text_out = str_result(result, tag)
       bench_file.write(text_out+"\n")
   
-  if day == 7:
-    bench_it('day07.soln1.realinput/part1', day07.soln1.part1,input1,100_000,True)
-    bench_it('day07.soln2.realinput/part2', day07.soln1.part2,input1,70_000_000, 30_000_000)
-  elif day == 12:
-    bench_it('day12.soln1.realinput/part1', day12.soln1.part1,input1)
-    bench_it('day12.soln1.realinput/part2', day12.soln1.part2,input1)
+  if day == 1:
+    bench_it('day01.soln1.realinput/part1', day01.soln1.part1,input1)
+    bench_it('day01.soln1.realinput/part2', day01.soln1.part2,input1)
+  else:
+    raise Exception(f"No benchmarks defined for day {day}")
 
   bench_file.close()
 
