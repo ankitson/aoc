4d6c152551b26366b9e204b48cf22ae92ed200c1
Ankit Soni <dev@ankitson.com>
2023-11-30 21:36:45 -0800

    2023-day01: finish part1 & part2

--- 2023/rust/day01/src/main.rs
+++ 2023/rust/day01/src/main.rs
@@ -14,8 +14,8 @@ pub fn main() {
 
     println!("Hello Day 01!");
     let input: &str = include_str!("../../inputs/sample01.txt");
-    let part1 = soln1::Soln1::part1(input);
-    println!("part1/sample1 = {:?}", part1);
+    // let part1 = soln1::Soln1::part1(input);
+    // println!("part1/sample1 = {:?}", part1);
 
     let input: &str = include_str!("../../inputs/day01.txt");
     let part1 = soln1::Soln1::part1(input);
