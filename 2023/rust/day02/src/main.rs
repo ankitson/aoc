19be34371679c0806f326a555dd92eaad84b3468
Ankit Soni <dev@ankitson.com>
2023-12-01 21:45:22 -0800

    2023-day02: basic optimization

--- 2023/rust/day02/src/main.rs
+++ 2023/rust/day02/src/main.rs
@@ -13,18 +13,22 @@ pub fn main() {
     let input: &str = include_str!("../../inputs/sample02.txt");
     let part1 = soln1::Soln1::part1(input);
     println!("part1/sample1 = {:?}", part1);
+    assert_eq!(part1, 8);
 
     let input: &str = include_str!("../../inputs/day02.txt");
     let part1 = soln1::Soln1::part1(input);
     println!("part1/day02 = {:?}", part1);
+    assert_eq!(part1, 2256);
 
     let input: &str = include_str!("../../inputs/sample02.txt");
     let part2 = soln1::Soln1::part2(input);
     println!("part2/sample02 = {:?}", part2);
+    assert_eq!(part2, 2286);
 
     let input: &str = include_str!("../../inputs/day02.txt");
     let part2 = soln1::Soln1::part2(input);
     println!("part2/day02 = {:?}", part2);
+    assert_eq!(part2, 74229);
 }
 
 #[cfg(test)]
--- 2023/rust/day02/src/soln1.rs
+++ 2023/rust/day02/src/soln1.rs
@@ -13,35 +13,35 @@ impl Soln1 {
         let mut possible: Vec<usize> = vec![];
         let mut idx = 1;
         for line in lines {
-            let gameparts = line.split(':').collect_vec();
-            let info = gameparts[1];
-            let splits = info.split(';').collect_vec();
             let mut is_valid = true;
+
+            let mut gameparts = line.split(':');
+            let info = gameparts.nth(1).unwrap();
+            let splits = info.split(';');
             for split in splits {
-                let results = split
-                    .split(',')
-                    .map(|x| x.trim())
-                    .map(|x| {
-                        let parts = x.split(' ').collect_vec();
-                        let pn = parts[0].parse::<usize>().unwrap();
-                        let pc = parts[1];
-                        match pc {
-                            "red" => (pn, 0, 0),
-                            "green" => (0, pn, 0),
-                            "blue" => (0, 0, pn),
-                            _ => panic!("illegal"),
+                split.split(',').for_each(|x| {
+                    let parts = x.trim().split(' ').collect_vec();
+                    let pn = parts[0].parse::<usize>().unwrap();
+                    let pc = parts[1];
+                    match pc {
+                        "red" => {
+                            if pn > 12 {
+                                is_valid = false
+                            }
                         }
-                    })
-                    .collect_vec();
-                let mut cresult = (0, 0, 0);
-                for result in results {
-                    cresult.0 += result.0;
-                    cresult.1 += result.1;
-                    cresult.2 += result.2;
-                }
-                if !(cresult.0 <= 12 && cresult.1 <= 13 && cresult.2 <= 14) {
-                    is_valid = false;
-                }
+                        "green" => {
+                            if pn > 13 {
+                                is_valid = false
+                            }
+                        }
+                        "blue" => {
+                            if pn > 14 {
+                                is_valid = false
+                            }
+                        }
+                        _ => panic!("illegal"),
+                    }
+                })
             }
             if is_valid {
                 possible.push(idx);
@@ -60,31 +60,22 @@ impl Soln1 {
         let lines = input.lines();
         let mut power: usize = 0;
         for line in lines {
-            let gameparts = line.split(':').collect_vec();
-            let info = gameparts[1];
-            let splits = info.split(';').collect_vec();
+            let mut gameparts = line.split(':');
+            let info = gameparts.nth(1).unwrap();
+            let splits = info.split(';');
             let mut cresult = (0, 0, 0);
             for split in splits {
-                let results = split
-                    .split(',')
-                    .map(|x| x.trim())
-                    .map(|x| {
-                        let parts = x.split(' ').collect_vec();
-                        let pn = parts[0].parse::<usize>().unwrap();
-                        let pc = parts[1];
-                        match pc {
-                            "red" => (pn, 0, 0),
-                            "green" => (0, pn, 0),
-                            "blue" => (0, 0, pn),
-                            _ => panic!("illegal"),
-                        }
-                    })
-                    .collect_vec();
-                for result in results {
-                    cresult.0 = result.0.max(cresult.0);
-                    cresult.1 = result.1.max(cresult.1);
-                    cresult.2 = result.2.max(cresult.2);
-                }
+                split.split(',').map(|x| x.trim()).for_each(|x| {
+                    let parts = x.split(' ').collect_vec();
+                    let pn = parts[0].parse::<usize>().unwrap();
+                    let pc = parts[1];
+                    match pc {
+                        "red" => cresult.0 = pn.max(cresult.0),
+                        "green" => cresult.1 = pn.max(cresult.1),
+                        "blue" => cresult.2 = pn.max(cresult.2),
+                        _ => panic!("illegal"),
+                    }
+                })
             }
             power += cresult.0 * cresult.1 * cresult.2;
         }
