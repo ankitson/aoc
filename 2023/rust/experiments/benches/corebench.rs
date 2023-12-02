d58b187420f18ca20f76b12c885664193f30c2e5
Ankit Soni <dev@ankitson.com>
2023-12-01 20:48:22 -0800

    2023: add pop_count

--- 2023/rust/experiments/benches/corebench.rs
+++ 2023/rust/experiments/benches/corebench.rs
@@ -1,10 +1,17 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use experiments::popcnt;
 
-pub fn popcnt_naive(c: &mut Criterion) {
-    let mut group = c.benchmark_group("experiments");
-    group.bench_function("popcnt_naive", |b| b.iter(|| popcnt::popcnt(0b10101010)));
+pub fn popcnt(c: &mut Criterion) {
+    let mut group = c.benchmark_group("popcnt");
+    group.bench_function("popcnt_naive", |b| {
+        b.iter(|| popcnt::popcnt_naive(0b10101010))
+    });
+
+    group.bench_function("popcnt_split", |b| {
+        b.iter(|| popcnt::popcnt_split(0b10101010))
+    });
     group.finish();
 }
-criterion_group!(benches, popcnt_naive);
+
+criterion_group!(benches, popcnt);
 criterion_main!(benches);
--- 2023/rust/experiments/src/popcnt.rs
+++ 2023/rust/experiments/src/popcnt.rs
@@ -1,7 +1,58 @@
-pub fn popcnt(mut x: u32) -> u32 {
+pub fn popcnt_naive(x: u32) -> u32 {
     let mut bits = [0; 32];
     for (i, bit) in bits.iter_mut().enumerate() {
         *bit = (x >> i) & 1;
     }
     bits.into_iter().fold(0, |total, bit| total + bit)
+}
+
+#[inline(never)]
+pub fn popcnt_split(mut x: u32) -> u32 {
+    let even = x & 0x55555555;
+    let odd = x & 0xaaaaaaaa;
+
+    x = even + (odd >> 1);
+
+    let even = x & 0x33333333;
+    let odd = x & 0xcccccccc;
+
+    x = even + (odd >> 2);
+
+    let even = x & 0x0f0f0f0f;
+    let odd = x & 0xf0f0f0f0;
+
+    x = even + (odd >> 4);
+
+    let even = x & 0x00ff00ff;
+    let odd = x & 0xff00ff00;
+
+    x = even + (odd >> 8);
+
+    let even = x & 0x0000ffff;
+    let odd = x & 0xffff0000;
+
+    x = even + (odd >> 16);
+
+    x
+}
+#[cfg(test)]
+mod tests {
+    use crate::popcnt::{popcnt_naive, popcnt_split};
+
+    #[test]
+    fn test_popcnt() {
+        let n1 = 0x10101010;
+        let n2 = 0x11111111;
+        let n3 = 0x00000000;
+        let n4 = 0xaaaa1111;
+
+        assert!(popcnt_naive(n1) == 4);
+        assert!(popcnt_naive(n2) == 8);
+        assert!(popcnt_naive(n3) == 0);
+        assert!(popcnt_naive(n4) == (2 * 4 + 4));
+        assert!(popcnt_split(n1) == 4);
+        assert!(popcnt_split(n2) == 8);
+        assert!(popcnt_split(n3) == 0);
+        assert!(popcnt_split(n4) == (2 * 4 + 4));
+    }
 }
