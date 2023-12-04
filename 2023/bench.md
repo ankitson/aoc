083c1aa0bc1bb066338faf08344f18aac0880927
Ankit Soni <dev@ankitson.com>
2023-12-03 21:34:38 -0800

    2023-day04: cleanup python soln

--- 2023/bench.md
+++ 2023/bench.md
@@ -15,7 +15,7 @@
 |rust.day03.part2.realinput/part2_fast             |    221807.52ns|       221.81µs|         0.22ms|          0.00s|
 |python.day03.soln1.realinput/part1                |   4784365.16ns|      4784.37µs|         4.78ms|          0.00s|
 |python.day03.soln1.realinput/part2                |   5070635.30ns|      5070.64µs|         5.07ms|          0.01s|
-|python.day04.soln1.realinput/part1                |   2900695.14ns|      2900.70µs|         2.90ms|          0.00s|
-|python.day04.soln1.realinput/part2                |   6258441.93ns|      6258.44µs|         6.26ms|          0.01s|
+|python.day04.soln1.realinput/part1                |   1457379.75ns|      1457.38µs|         1.46ms|          0.00s|
+|python.day04.soln1.realinput/part2                |   1555943.55ns|      1555.94µs|         1.56ms|          0.00s|
 |rust.TOTAL                                        |    760565.08ns|       760.57µs|         0.76ms|          0.00s|
-|python.TOTAL                                      |  19014137.53ns|     19014.14µs|        19.01ms|          0.02s|
+|python.TOTAL                                      |  12868323.75ns|     12868.32µs|        12.87ms|          0.01s|
--- 2023/python/day04/soln1.py
+++ 2023/python/day04/soln1.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-import copy
 
 # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 # Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -11,7 +10,6 @@ def parse(raw_input):
   lines = [line for line in raw_input.split("\n") if len(line) > 1]
   cards = []
   for line in lines:
-    # print(f"line: {line}")
     parts1 = line.split(":")
     nums = parts1[1]
     parts2 = nums.split("|")
@@ -19,9 +17,6 @@ def parse(raw_input):
     have = [int(x.strip()) for x in parts2[1].split(" ") if len(x) > 0]
     cards.append((winning,have))
   return cards
-
-
-  # raise Exception("not implemented!")
 
 def part1(input):
   cards = parse(input)
@@ -34,58 +29,38 @@ def part1(input):
           card_points = 1
         else:
           card_points *= 2
-    print(f"Card: {winning} {have} points = {card_points}")
     points += card_points
   return points
-    
-        
-  print(cards)
-  raise Exception("not implemented!")
 
 def part2(input):
   cards = parse(input)
-  total = 0
   card_counts = defaultdict(int) #1 - 180
   for i in range(0,len(cards)):
     card_counts[i] = 1
   
-  round = 0
-  prev_counts = copy.deepcopy(card_counts)
-  # while True:
-  print(f"before round {round}:\n\tcards = {card_counts}")
   for card_num, count in card_counts.items():
     (winning, have) = cards[card_num]
     num_match = len(set(winning) & set(have)) #Card 0 has 4 matches
-    print(f"\t\t{num_match} matching for {card_num}")
     for i in range(num_match):
-      print(f"\t\tadding {count} to card {card_num+i+1}")
       card_counts[card_num+i+1] += count #Card 1,2,3,4
-    # if card_counts == prev_counts:
-      # break
-    # prev_counts = card_counts
-    # round += 1
-  
   return sum(card_counts.values())
-  return True
-    
-  
-
-  
-  # for (card)
-  # raise Exception("not implemented!")
 
 def main(sample, input1):
   print(f"Hello Day 04!")
 
   soln = part1(sample)
   print(f"Part 1 (sample) = {soln}")
+  assert(soln == 13)
 
   soln = part1(input1)
   print(f"Part 1 (realinput) = {soln}")
+  assert(soln == 19135)
 
   soln = part2(sample)
   print(f"Part 2 (sample) = {soln}")
+  assert(soln == 30)
 
   soln = part2(input1)
   print(f"Part 2 (realinput) = {soln}")
+  assert(soln == 5704953)
 
