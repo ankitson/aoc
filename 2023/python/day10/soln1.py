e058caaf0ae21b9743f0b3932f814f1d9544ee7b
Ankit Soni <dev@ankitson.com>
2023-12-10 09:26:45 -0800

    2023-day10: p2 done math enjoyers winning

--- 2023/python/day10/soln1.py
+++ 2023/python/day10/soln1.py
@@ -3,6 +3,17 @@ import math, itertools, sys
 from collections import defaultdict, deque
 import time
 
+typ = {
+    '|': 'UD',
+    '-' : 'LR',
+    'L' : 'UR',
+    'J' : 'UL',
+    '7' : 'DL',
+    'F' : 'DR',
+    '.' : 'G',
+    'S' : 'S'
+  }
+
 def nbrs(grid, x, y):
   cands = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 
@@ -120,27 +131,22 @@ def bfs(nodes, start, stdscr, fps_cap, fps):
 
 
 def stack_dfs(nodes, start):
-  stack = [(start, 0, [])]  # Initialize stack with start node, depth 0, and empty path
-  seen = set()  # Initialize set to keep track of visited nodes
-
+  stack = [(start, 0, [])]
+  seen = set()
   while stack:
-    curr, depth, path = stack.pop()  # Pop the top node from the stack
+    curr, depth, path = stack.pop() 
     cx, cy = curr
-
     if (cx, cy) == start and depth > 0:  # We made it back to start
       return depth, path
-
     if (cx, cy) in seen:
       continue
 
     seen.add((cx, cy))
     mynbrs = nbrs(nodes, cx, cy)
-
     for nbdst, nbr in mynbrs:
       if nbr not in seen or (nbr == start and depth > 1):
-        stack.append((nbr, depth + 1, path + [nbr]))  # Push neighbor node to stack with increased depth and updated path
-
-  return float('-inf'), []  # If no path found, return negative infinity and empty path
+        stack.append((nbr, depth + 1, path + [nbr]))  # type: ignore
+  return float('-inf'), []  # No path
 
 def dfs(nodes, curr, start, seen, depth=0):
   print(f"curr = {curr}, start = {start}, depth = {depth}")
@@ -169,8 +175,6 @@ def parse(raw_input):
       if lines[i][j] == 'S':
         start_idx = (i,j)
         break
-  
-
   return (lines,start_idx)
 
 def part1(input):
@@ -182,41 +186,189 @@ def part1(input):
   print(path)
   return dist // 2
 
-def part2(input):
+def part2_picks(input):
   grid,start_idx = parse(input)
   (dist,path) = stack_dfs(grid, start_idx)
-  points_x = defaultdict(list)
-  for (x,y) in path:
-    points_x[x].append(y)
-  for row in range(len(grid)):
-    points_x[row].sort()
-  
-  for row in range(len(grid)):
-    for col in range(len(grid[row])):
-      if (row,col) in path:
-        print('X', end='')
-      else:
-        print(grid[row][col], end='')
-    print("")
-
   area = 0
-  for row in range(len(grid)):
-    area_row = 0
-    # print(f"points {row} = {points_x[row]}")
-    inside = True
-    for (y1,y2) in itertools.pairwise(points_x[row]):
-      print(f"\t {y1}-{y2} {inside}")
-      # print(f"inside = {inside}")
-      if inside:
-        #FJL7L7LJLJ||LJIL-7OO
-        #01234567890123456789
-        dots = len([grid[row][y] for y in range(y1+1,y2) if grid[row][y] == '.'])
-        # print(f"{dots} points between ({y1}, {y2})")
-        area_row += dots
-      inside = not inside
-    area += area_row
-    print(f"{row}: {area_row}")
-  return area
+  # Shoelace formula
+  for ((x1,y1),(x2,y2)) in itertools.pairwise(path):
+    area += (y1+y2) * (x2-x1)
+  if area < 0: #we went clockwise so recompute going anti clockwise
+    area = 0
+    for ((x1,y1),(x2,y2)) in itertools.pairwise(path):
+      area += (y1+y2) * (x1-x2)
+
+  # Picks Theorem: A = i + b/2 - 1
+  area = area // 2
+  interior = area - len(path) // 2 + 1
+  return interior
+
+# def part2(input):
+#   grid,start_idx = parse(input)
+#   (dist,path) = stack_dfs(grid, start_idx)
+#   points_x = defaultdict(list)
+#   points_y = defaultdict(list)
+#   for (x,y) in path:
+#     points_x[x].append(y)
+#     points_y[y].append(x)
+#   for row in range(len(grid)):
+#     points_x[row].sort()
+#   for col in range(len(grid[0])):
+#     points_y[col].sort()
+  
+  
+
+
+#   (px,py) = path[-2]
+#   (sx,sy) = path[-1]
+#   (pdx,pdy) = (sx-px,sy-py)
+#   (nx,ny) = path[0]
+#   (ndx,ndy) = (nx-sx,ny-sy)
+
+#   #prev = 40,40
+#   #curr = 40,41
+#   #next = 39,41
+
+#   #pdy = 1 => start has a entry from below
+#   #ndy = 1 => start faces above
+
+#   cands = ['L','7','F','J','|','-']
+#   #figure out which type the start node is
+#   #it should be symmetric over (px,py) and (nx,ny) because it is a junction
+#   if pdy == 1 and ndy == 1 or (pdy == -1 and ndy == -1):
+#     path_pipe = '|'
+#   elif (pdx == 1 and ndx == 1) or (pdx == -1 and ndx == -1):
+#     path_pipe = '-'
+#   elif (pdx == 1 and ndy == 1) or (ndx == 1 and pdy == 1):
+#     path_pipe = 'J'
+#   elif (pdx == -1 and ndy == -1) or (ndx == -1 and pdy == -1):
+#     path_pipe = '7'
+#   elif (pdx == 1 and ndy == -1) or (ndx == 1 and pdy == -1):
+#     path_pipe = 'L'
+#   elif (pdx == -1 and ndy == 1) or (ndx == -1 and pdy == 1):
+#     path_pipe = 'F'
+#   else:
+#     raise Exception("wat")
+
+
+
+
+
+  
+#   # above = 
+#   # below = (sx+1,sy)
+#   # left = (sx,sy-1)
+#   # right = (sx,sy+1)
+#   # path_pipe = '|'
+#   # if prev 
+#   #   path_pipe = 'J'
+#   # elif above in path and right in path:
+#   #   path_pipe = 'L'
+#   # elif below in path and left in path:
+#   #   path_pipe = '7'
+#   # elif below in path and right in path:
+#   #   path_pipe = 'F'
+#   # elif above in path or below in path:
+#   #   path_pipe = '|'
+#   # elif left in path or right in path:
+#   #   path_pipe = '-'
+#   # else:
+#   #   raise Exception("wat")
+  
+#   grid[sx][sy] = path_pipe
+
+#   for row in range(len(grid)):
+#     for col in range(len(grid[row])):
+#       if (row,col) in path:
+#         print(grid[row][col], end='')
+#       else:
+#         print('.', end='')
+#     print("")
+
+#   areas = []
+#   for row in range(len(grid)):
+#     area_row = 0
+#     inside = False
+#     prev = None
+#     for y in range(len(grid[row])):
+#       # prev = grid[row][y-1] if y > 0 else None
+#       pipe = grid[row][y]
+#       # if pipe == 'S':
+#         # inside = True
+#         # pipe = path_pipe
+#         # print("replace start with")
+#       # if not (row,y) in path:
+#       #   continue
+#       if not (row,y) in path:
+#         pipe = '.'
+      
+
+#       # if pipe in ['L','F'] and prev in ['7','J']:
+#         # inside = not inside
+#       if pipe in ['7','J']:
+#         assert prev in ['L','F', '|', None]
+#         if prev in ['L','F',None]:
+#           inside = not inside
+#       elif pipe in ['L','F']:
+#         assert prev in ['7','J', '|', None]
+#         if prev in ['7','J',None]:
+#           inside = not inside
+#       elif pipe == '|':
+#         inside = not inside
+
+#       if pipe == '.' and inside:
+#         # print(f"{row},{y} is inside")
+#         area_row += 1
+#       # face right - L, F
+#       # face left - 7, J
+#       # prev faces right, then the current must face left if its on the path
+#       # if 
+      
+      
+#       if not pipe in ['.', '-']:
+#         prev = pipe
+
+#       # elif pipe != '.':
+#       #   inside = not inside
+#       # if grid[row][y] == '.' and inside:
+#       #     print(f"{row},{y} is inside")
+#       #     area_row += 1
+#     areas.append(area_row)
+#     # for y in range(len(grid[row])-2,-1,-1):
+
+#     #   # print(f"\ty = {y}")
+#     #   if y in points_x[row] and grid[row][y] != 'S':
+#     #     # print(f"\ty is in path")
+#     #     to_right += 1
+#     #   elif grid[row][y] == '.':
+#     #     print(f"\ty = {y} match",end="")
+#     #     # print(f"|{y} . - right = {to_right}",end="")
+#     #     if to_right % 2 == 1 and not (y == 0):
+#     #       area_row += 1
+#     # areas.append(area_row)
+#   print(f"\nareas = {areas}")
+#   return sum(areas)
+
+
+    
+
+  #   area_row = 0
+  #   inside = False
+  #   for (y1,y2) in itertools.pairwise(points_x[row]):
+  #     if not typ[grid[row][y1]] == 'LR':
+  #       inside = not inside
+  #     if inside:
+  #       for y in range(y1+1, y2):
+  #         area_row += 1
+  #   areas.append(area_row)
+  #   area += area_row
+  #   # print(f"row {row} area = {area_row}")
+  # print(f"areas = {areas}")
+  # return area
+        # dots = len([grid[row][y] for y in range(y1+1,y2) if grid[row][y] == '.'])
+        # area_row += dots
+    # area += area_row
+  # return area
 
 
   
@@ -268,7 +420,7 @@ def part2(input):
 
   raise Exception("not implemented!")
 
-def main(sample, sample2, sample3, sample4, sample5, sample6, sample7, input1):
+def main(sample, sample2, sample3, sample4, sample5, sample6, input1):
   print(f"Hello Day 10!")
 
   soln = part1(sample)
@@ -280,15 +432,16 @@ def main(sample, sample2, sample3, sample4, sample5, sample6, sample7, input1):
   soln = part1(input1)
   print(f"Part 1 (realinput) = {soln}")
 
-  soln = part2(sample5)
+  soln = part2_picks(sample5)
   print(f"Part 2 (sample5) = {soln}")
-  soln = part2(sample6)
+  soln = part2_picks(sample6)
   print(f"Part 2 (sample6) = {soln}")
-  soln = part2(sample7)
-  print(f"Part 2 (sample7) = {soln}")
-
-  # soln = part2(input1)
-  # print(f"Part 2 (realinput) = {soln}")
+  soln = part2_picks(sample3)
+  print(f"Part 2 (sample3) = {soln}")
+  soln = part2_picks(sample4)
+  print(f"Part 2 (sample4) = {soln}")
+  soln = part2_picks(input1)
+  print(f"Part 2 (realinput) = {soln}")
 
 def wait_for_anykey(stdscr):
   stdscr.nodelay(False)
