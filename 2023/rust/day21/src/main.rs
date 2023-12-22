d28dfd188881e56a11ae706caea78679b5aa8413
Ankit Soni <dev@ankitson.com>
2023-12-21 20:57:46 -0800

    2023-day21: Finish P1 & P2

--- 2023/rust/day21/src/main.rs
+++ 2023/rust/day21/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(let_chains)]
 mod shared;
 mod soln1;
 
@@ -12,23 +13,27 @@ static ALLOC: dhat::Alloc = dhat::Alloc;
 pub fn main() {
     #[cfg(feature = "heapprofile")]
     let _profiler = dhat::Profiler::new_heap();
-
-    println!("Hello Day 21!");
-    // let input: &str = include_str!("../../inputs/sample21.txt");
-    // let part1 = soln1::part1(input);
-    // println!("part1/sample1 = {:?}", part1);
-
-    // let input: &str = include_str!("../../inputs/day21.txt");
-    // let part1 = soln1::part1(input);
-    // println!("part1/day21 = {:?}", part1);
-
-    let input: &str = include_str!("../../inputs/sample21.txt");
-    let part2 = soln1::part2(input);
-    println!("part2/sample21 = {:?}", part2);
-
+    let sample: &str = include_str!("../../inputs/sample21.txt");
     let input: &str = include_str!("../../inputs/day21.txt");
-    let part2 = soln1::part2(input);
-    println!("part2/day21 = {:?}", part2);
+    println!("Hello Day 21!");
+
+    let part1 = soln1::part1(sample, 6);
+    println!("part1/sample1 = {:?}", part1);
+    assert_eq!(part1, 16);
+
+    let part1 = soln1::part1(input, 64);
+    println!("part1/day21 = {:?}", part1);
+    assert_eq!(part1, 3532);
+
+    for (nstep, exp) in [(6, 16), (10, 50), (50, 1594), (100, 6536)] {
+        let part2 = soln1::part2(sample, nstep, false);
+        println!("part2/sample1({nstep}) = {:?}", part2);
+        assert_eq!(part2, exp);
+    }
+
+    println!("Part 2 - here are the first 200 values of the function. Use the python notebook for the rest");
+    const NSTEPS_P2: usize = 26501365;
+    let part2 = soln1::part2(input, 200, true);
 }
 
 #[cfg(test)]
--- 2023/rust/day21/src/soln1.rs
+++ 2023/rust/day21/src/soln1.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use itertools::Itertools;
 use regex::Regex;
@@ -19,121 +19,41 @@ pub fn parse(input: &str) -> (Vec<Vec<char>>, (usize, usize)) {
     (grid, start)
 }
 
-fn nbrs(x: usize, y: usize, hts: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
-    let max_x = hts.len();
-    let max_y = hts[0].len();
-    let cands = vec![
-        // (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() - 1),
-        (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() + 0),
-        // (i64::try_from(x).unwrap() - 1, i64::try_from(y).unwrap() + 1),
-        (i64::try_from(x).unwrap() + 0, i64::try_from(y).unwrap() - 1),
-        (i64::try_from(x).unwrap() + 0, i64::try_from(y).unwrap() + 1),
-        // (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() - 1),
-        (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() + 0),
-        // (i64::try_from(x).unwrap() + 1, i64::try_from(y).unwrap() + 1),
-    ];
-
-    let mut nbrs2: Vec<(usize, usize)> = Vec::new();
-    for (a, b) in cands {
-        if a < 0 || b < 0 {
-            continue;
-        }
-        let aa = a.try_into().unwrap();
-        let bb = b.try_into().unwrap();
-        if aa < max_x && bb < max_y {
-            let ro: &Vec<char> = &hts[aa];
-            let pt = ro[bb];
-            if (pt == '.' || pt == 'S') {
-                nbrs2.push((aa, bb));
-            }
-        }
-    }
-    nbrs2
-}
-
-pub fn part1(raw_input: &str) -> Output {
+pub fn part1(raw_input: &str, nsteps: usize) -> Output {
     let (grid, start) = parse(raw_input);
     let mut to_visit = VecDeque::from([start]);
     let mut next = VecDeque::new();
-    let mut seen = HashSet::new();
     let mut seen_per = HashSet::new();
     let mut steps = 0;
-    let NSTEPS = 64 + 1;
-    while (to_visit.len() > 0 && steps < NSTEPS) {
+    while to_visit.len() > 0 && steps <= nsteps {
         let (vr, vc) = to_visit.pop_back().unwrap();
-        seen.insert((vr, vc));
         seen_per.insert((vr, vc));
-        for (nr, nc) in nbrs(vr, vc, &grid) {
-            if !seen_per.contains(&(nr, nc)) && !next.contains(&(nr, nc)) {
-                next.push_back((nr, nc));
+        for (dr, dc) in [(0, -1), (-1, 0), (1, 0), (0, 1)] {
+            let nr = (vr as isize) + dr;
+            let nc = (vc as isize) + dc;
+            if nr.min(nc) >= 0
+                && nr < grid.len() as isize
+                && nc < grid[0].len() as isize
+                && grid[nr as usize][nc as usize] != '#'
+            {
+                let nr = nr as usize;
+                let nc = nc as usize;
+                if !seen_per.contains(&(nr, nc)) && !next.contains(&(nr, nc)) {
+                    next.push_back((nr, nc));
+                }
             }
         }
-        if to_visit.len() == 0 {
+        if to_visit.len() == 0 && steps < nsteps {
             to_visit = next;
             next = VecDeque::new();
             steps += 1;
-            let nseen = seen_per.len();
-            println!("{nseen} seen after steps {steps}");
-            // println!("next layer = {to_visit:?}");
-            if steps == NSTEPS {
-                break;
-            }
             seen_per = HashSet::new();
         }
     }
     seen_per.len()
 }
 
-fn pos_mod(a: isize, b: isize) -> usize {
-    (((a % b) + b) % b) as usize
-}
-
-fn how_many(grid: &Vec<Vec<char>>, starts: &Vec<(isize, isize)>) -> (usize, Vec<(isize, isize)>) {
-    let mut to_visit = VecDeque::from(starts.clone());
-    let mut next = VecDeque::new();
-    let mut seen = HashSet::new();
-    let mut overflows = vec![];
-    let mut nsteps = 1;
-    while to_visit.len() > 0 {
-        let (vr, vc) = to_visit.pop_back().unwrap();
-        seen.insert((vr as isize, vc as isize));
-        for (dr, dc) in [(0, -1), (0, 1), (1, 0), (-1, 0)] {
-            let nr = (vr as isize) + dr;
-            let nc = (vc as isize) + dc;
-            if (nr < 0 || nc < 0 || nr >= grid.len() as isize || nc >= grid[0].len() as isize) {
-                overflows.push((nr, nc));
-            } else {
-                if grid[nr as usize][nc as usize] == '.' || grid[nr as usize][nc as usize] == 'S' {
-                    if !seen.contains(&(nr, nc)) && !next.contains(&(nr, nc)) {
-                        next.push_back((nr as isize, nc as isize));
-                    }
-                }
-            }
-        }
-        if to_visit.len() == 0 {
-            for i in 0..grid.len() {
-                for j in 0..grid[0].len() {
-                    if seen.contains(&(i as isize, j as isize)) && grid[i][j] == '.' {
-                        print!("X");
-                    } else {
-                        print!("{}", grid[i][j]);
-                    }
-                }
-                println!();
-            }
-            println!("----------------------");
-            if overflows.len() > 0 {
-                return (nsteps, overflows);
-            }
-            to_visit = next;
-            next = VecDeque::new();
-            nsteps += 1
-        }
-    }
-    unreachable!()
-}
-
-pub fn part2(raw_input: &str) -> Output {
+pub fn part2(raw_input: &str, nsteps: usize, print_intermediate: bool) -> Output {
     let (grid, start) = parse(raw_input);
     let (sr, sc) = (start.0 as isize, start.1 as isize);
     let mut to_visit = VecDeque::from([(sr, sc)]);
@@ -141,64 +61,36 @@ pub fn part2(raw_input: &str) -> Output {
     let mut seen: HashSet<(isize, isize)> = HashSet::new();
     let mut seen_per: HashSet<(isize, isize)> = HashSet::new();
     let mut steps = 0;
-    let NSTEPS = 26501365 + 1;
 
-    /*
-     * for grid and start pos, we have dp[start][steps] = how many i have seen at exactly steps steps starting from start in grid
-     *
-     * start at 5,5
-     * expand.
-     * when i hit a boundary at step K and overflow. it maps to a copy of grid with diff start
-     * so dp[start][K+1] = dp[start][K] + dp[new_start][1]
-     *
-     */
-
-    let (nsteps, oversteps) = how_many(&grid, &vec![(sr, sc)]);
-    println!("After {nsteps} from {sr} {sc}, we overstep at {oversteps:?} ");
-    todo!()
-
-    // let mut prev_values = vec![];
-    // while to_visit.len() > 0 && steps < NSTEPS {
-    //     let (vr, vc) = to_visit.pop_back().unwrap();
-    //     seen.insert((vr as isize, vc as isize));
-    //     seen_per.insert((vr as isize, vc as isize));
-    //     for (dr, dc) in [(0, -1), (0, 1), (1, 0), (-1, 0)] {
-    //         let nrm = pos_mod((vr as isize) + dr, grid.len() as isize);
-    //         let ncm = pos_mod((vc as isize) + dc, grid[0].len() as isize);
-    //         let nr = (vr as isize) + dr;
-    //         let nc = (vc as isize) + dc;
-    //         if grid[nrm][ncm] == '.' || grid[nrm][ncm] == 'S' {
-    //             if !seen_per.contains(&(nr, nc)) && !next.contains(&(nr, nc)) {
-    //                 next.push_back((nr, nc));
-    //             }
-    //         }
-    //     }
-    //     // for (nr, nc) in nbrs(vr, vc, &grid) {
-
-    //     // }
-    //     if to_visit.len() == 0 {
-    //         to_visit = next;
-    //         next = VecDeque::new();
-    //         steps += 1;
-    //         let nseen = seen_per.len();
-    //         println!("{nseen} seen after steps {steps}");
-
-    //         //MINE IS N+1, AFTER STPES = 101 corresponds to after 100
-    //         // println!("next layer = {to_visit:?}");
-    //         if steps == NSTEPS {
-    //             break;
-    //         }
-
-    //         let prev_position = prev_values.iter().find_position(|seen_set| **seen_set == seen_per);
-    //         if let Some((earlier_idx, earlier_set)) = prev_position {
-    //             println!("after {steps}, repeated at {earlier_idx}");
-
-    //             panic!();
-    //         }
-    //         prev_values.push(seen_per.clone());
-
-    //         seen_per = HashSet::new();
-    //     }
-    // }
-    // seen_per.len()
+    while to_visit.len() > 0 && steps <= nsteps {
+        let (vr, vc) = to_visit.pop_back().unwrap();
+        seen.insert((vr as isize, vc as isize));
+        seen_per.insert((vr as isize, vc as isize));
+        for (dr, dc) in [(0, -1), (0, 1), (1, 0), (-1, 0)] {
+            let nr = (vr as isize) + dr;
+            let nc = (vc as isize) + dc;
+            let nrm = nr.rem_euclid(grid.len() as isize) as usize;
+            let ncm = nc.rem_euclid(grid[0].len() as isize) as usize;
+            if grid[nrm][ncm] == '.' || grid[nrm][ncm] == 'S' {
+                if !seen_per.contains(&(nr, nc)) && !next.contains(&(nr, nc)) {
+                    next.push_back((nr, nc));
+                }
+            }
+        }
+        if to_visit.len() == 0 && steps < nsteps {
+            to_visit = next;
+            next = VecDeque::new();
+            let nseen = seen_per.len();
+            if print_intermediate {
+                println!("({steps},{nseen}),");
+            }
+            steps += 1;
+            seen_per = HashSet::new();
+        }
+    }
+    let nseen = seen_per.len();
+    if print_intermediate {
+        println!("({nsteps},{nseen})");
+    }
+    seen_per.len()
 }
