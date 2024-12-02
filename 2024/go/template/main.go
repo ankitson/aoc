852e4bf5f7f679340eaca1948298a833a3956539
Ankit Soni <dev@ankitson.com>
2024-12-01 20:53:09 -0800

    basic go template

---
+++ 2024/go/template/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func assertEqual[T comparable](got, want T) {
+	if got != want {
+		fmt.Printf("Error: got %v, want %v\n", got, want)
+	}
+}
+
+type Input = string
+type Output = string
+
+func parse(input []byte) Input {
+	panic("unimplemented")
+}
+
+func part1(input []byte) Output {
+	panic("unimplemented")
+}
+
+func part2(input []byte) Output {
+	panic("unimplemented")
+}
+
+func main() {
+	sample1, err := os.ReadFile("./inputs/sample02.txt")
+	check(err)
+	inp1, err := os.ReadFile("./inputs/day02.txt")
+	check(err)
+
+	fmt.Println("Welcome to Day 2!!")
+	ans_sample_p1 := part1(sample1)
+	fmt.Printf("part1/sample1 = %d\n", ans_sample_p1)
+	assertEqual(ans_sample_p1, "")
+
+	ans_p1 := part1(inp1)
+	fmt.Printf("part1/day01 = %d\n", ans_p1)
+	assertEqual(ans_p1, "")
+
+	ans_sample_p2 := part2(sample1)
+	fmt.Printf("part2/sample1 = %d\n", ans_sample_p2)
+	assertEqual(ans_sample_p2, "")
+
+	ans_p2 := part2(inp1)
+	fmt.Printf("part2/day01 = %d\n", ans_p2)
+	assertEqual(ans_p2, "")
+}
