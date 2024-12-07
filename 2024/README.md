be3809c5d99b21309a92d71e58ec904f07a0898d
Ankit Soni <dev@ankitson.com>
2024-12-06 18:01:50 -0800

    2024-day06: cleanup and optimize. p1-2ms,p2-3s

--- 2024/README.md
+++ 2024/README.md
@@ -20,9 +20,9 @@ We use `Justfiles` liberally to perform common tasks. You can run commands from 
 |rust.day04.part1.realinput/part1                  |        6506761.48ns|           6506.76µs|         6.51ms|          0.01s|
 |rust.day04.part2.realinput/part2                  |         133482.43ns|            133.48µs|         0.13ms|          0.00s|
 |rust.day05.part1.realinput/part1                  |         160490.13ns|            160.49µs|         0.16ms|          0.00s|
-|rust.day06.part1.realinput/part1                  |        2344780.70ns|           2344.78µs|         2.34ms|          0.00s|
-|rust.day06.part2.realinput/part2                  |    12863896562.70ns|       12863896.56µs|     12863.90ms|         12.86s|
-|rust.TOTAL                                        |    12874808140.72ns|       12874808.14µs|     12874.81ms|         12.87s|
+|rust.day06.part1.realinput/part1                  |        2303737.82ns|           2303.74µs|         2.30ms|          0.00s|
+|rust.day06.part2.realinput/part2                  |     3240683859.00ns|        3240683.86µs|      3240.68ms|          3.24s|
+|rust.TOTAL                                        |     3251554394.14ns|        3251554.39µs|      3251.55ms|          3.25s|
 <tend></tend>
 
 ## Fetching Puzzles
--- 2024/rust/day06/src/soln1.rs
+++ 2024/rust/day06/src/soln1.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
+use rayon::prelude::*;
 use regex::Regex;
 
 pub type Input = (HashMap<(isize, isize), char>, (isize, isize), usize, usize);
@@ -23,54 +24,149 @@ pub fn parse(input: &str) -> Input {
     (grid, start, nr + 1, nc + 1)
 }
 
-fn rot((dr, dc): (isize, isize)) -> (isize, isize) {
-    match (dr, dc) {
-        (-1, 0) => (0, 1),
-        (0, 1) => (1, 0),
-        (1, 0) => (0, -1),
-        (0, -1) => (-1, 0),
-        _ => panic!("illegal dir"),
-    }
-}
-
-pub fn part1(raw_input: &str) -> Output {
-    let (mut grid_map, (sr, sc), numrows, numcols) = parse(raw_input);
-    let mut visited = 0;
-    let (mut cr, mut cc) = (sr, sc);
-    let (mut dr, mut dc) = match (grid_map.get(&(cr, cc)).unwrap()) {
+fn arrow_to_dir(ch: char) -> (isize, isize) {
+    return match ch {
         '^' => (-1isize, 0),
         '>' => (0, 1),
         'v' => (1, 0),
         '<' => (0, -1isize),
         _ => panic!("illegal dir"),
     };
+}
 
-    let mut visited = HashSet::new();
-    visited.insert((cr, cc));
-    grid_map.entry((cr, cc)).insert_entry('.');
+fn rot((dr, dc): (isize, isize)) -> (isize, isize) {
+    return (dc, -dr); //shortcut
+}
+
+pub fn tour_grid(
+    grid_map: &HashMap<(isize, isize), char>,
+    mut cr: isize,
+    mut cc: isize,
+    mut dr: isize,
+    mut dc: isize,
+) -> HashSet<(isize, isize)> {
+    let mut visited: HashSet<(isize, isize)> = HashSet::from_iter([(cr, cc)].into_iter());
     while grid_map.contains_key(&(cr, cc)) {
         visited.insert((cr, cc));
         let (nr, nc) = (cr + dr, cc + dc);
         if !grid_map.contains_key(&(nr, nc)) {
             break;
         }
-        let at_loc = grid_map.get(&(nr, nc)).unwrap();
-        if *at_loc == '.' {
+        let &next = grid_map.get(&(nr, nc)).unwrap();
+        if next == '.' {
             (cr, cc) = (nr, nc);
             continue;
-        } else if *at_loc == '#' {
+        } else if next == '#' {
             (dr, dc) = rot((dr, dc))
         } else {
             panic!("illegal grid char")
         }
     }
-
-    visited.len()
+    visited
 }
+
+pub fn part1(raw_input: &str) -> Output {
+    let (mut grid_map, (sr, sc), _, _) = parse(raw_input);
+    let (dr, dc) = arrow_to_dir(*grid_map.get(&(sr, sc)).unwrap());
+    grid_map.entry((sr, sc)).insert_entry('.');
+    let visited_locs = tour_grid(&grid_map, sr, sc, dr, dc);
+    visited_locs.len()
+}
+
+//bruteforce
+pub fn part2(raw_input: &str) -> Output {
+    let (mut grid_map, (sr, sc), _, _) = parse(raw_input);
+    let (mut dr, mut dc) = arrow_to_dir(*grid_map.get(&(sr, sc)).unwrap());
+    grid_map.entry((sr, sc)).insert_entry('.');
+    let visited_locs = tour_grid(&grid_map, sr, sc, dr, dc);
+
+    let (mut cr, mut cc) = (sr, sc);
+    let (sdr, sdc) = (dr, dc);
+
+    let mut causes_loop = HashSet::new();
+    for (rownum, colnum) in visited_locs {
+        if (rownum, colnum) != (sr, sc) && *grid_map.get(&(rownum, colnum)).unwrap() == '.' {
+            grid_map.entry((rownum, colnum)).insert_entry('#');
+            (cr, cc) = (sr, sc);
+            (dr, dc) = (sdr, sdc);
+            let mut visited = HashSet::new();
+            'outer: while grid_map.contains_key(&(cr, cc)) {
+                if visited.contains(&(cr, cc, dr, dc)) {
+                    causes_loop.insert((rownum, colnum));
+                    break 'outer;
+                }
+                visited.insert((cr, cc, dr, dc));
+                let (nr, nc) = (cr + dr, cc + dc);
+                if !grid_map.contains_key(&(nr, nc)) {
+                    break;
+                }
+                let next = grid_map.get(&(nr, nc)).unwrap();
+                if *next == '.' {
+                    (cr, cc) = (nr, nc);
+                    continue;
+                } else if *next == '#' {
+                    (dr, dc) = rot((dr, dc))
+                } else {
+                    panic!("illegal grid char")
+                }
+            }
+            grid_map.entry((rownum, colnum)).insert_entry('.');
+        }
+    }
+    causes_loop.len()
+}
+
+// pub fn part2_par(raw_input: &str) -> Output {
+//     let (mut grid_map, (sr, sc), numrows, numcols) = parse(raw_input);
+//     let (mut cr, mut cc) = (sr, sc);
+//     let (mut dr, mut dc) = match (grid_map.get(&(cr, cc)).unwrap()) {
+//         '^' => (-1isize, 0),
+//         '>' => (0, 1),
+//         'v' => (1, 0),
+//         '<' => (0, -1isize),
+//         _ => panic!("illegal dir"),
+//     };
+//     let (sdr, sdc) = (dr, dc);
+
+//     grid_map.entry((cr, cc)).insert_entry('.');
+//     let mut causes_loop = HashSet::new();
+//     (0..numrows.try_into().unwrap()).into_par_iter().for_each(|rownum| {
+//         for colnum in 0..numcols.try_into().unwrap() {
+//             if (rownum, colnum) != (sr, sc) && *grid_map.get(&(rownum, colnum)).unwrap() == '.' {
+//                 grid_map.entry((rownum, colnum)).insert_entry('#');
+//                 (cr, cc) = (sr, sc);
+//                 (dr, dc) = (sdr, sdc);
+//                 let mut visited = HashSet::new();
+//                 'outer: while grid_map.contains_key(&(cr, cc)) {
+//                     if visited.contains(&(cr, cc, dr, dc)) {
+//                         causes_loop.insert((rownum, colnum));
+//                         break 'outer;
+//                     }
+//                     visited.insert((cr, cc, dr, dc));
+//                     let (nr, nc) = (cr + dr, cc + dc);
+//                     if !grid_map.contains_key(&(nr, nc)) {
+//                         break;
+//                     }
+//                     let at_loc = grid_map.get(&(nr, nc)).unwrap();
+//                     if *at_loc == '.' {
+//                         (cr, cc) = (nr, nc);
+//                         continue;
+//                     } else if *at_loc == '#' {
+//                         (dr, dc) = rot((dr, dc))
+//                     } else {
+//                         panic!("illegal grid char")
+//                     }
+//                 }
+//                 grid_map.entry((rownum, colnum)).insert_entry('.');
+//             }
+//         }
+//     });
+//     causes_loop.len()
+// }
 
 //it only catches cycles if i immediately enter them after turning right, but not later
 pub fn part2_wrong(raw_input: &str) -> Output {
-    let (mut grid_map, (sr, sc), numrows, numcols) = parse(raw_input);
+    let (mut grid_map, (sr, sc), _, _) = parse(raw_input);
     let (mut cr, mut cc) = (sr, sc);
     let (mut dr, mut dc) = match (grid_map.get(&(cr, cc)).unwrap()) {
         '^' => (-1isize, 0),
@@ -81,7 +177,6 @@ pub fn part2_wrong(raw_input: &str) -> Output {
     };
 
     let mut visited = HashSet::new();
-    // visited.insert((cr, cc));
     let mut last_loc = (cr, cc);
     grid_map.entry((cr, cc)).insert_entry('.');
     let mut causes_loop = HashSet::new();
@@ -109,56 +204,5 @@ pub fn part2_wrong(raw_input: &str) -> Output {
         }
     }
     println!("causes loop = {:?}", causes_loop);
-    causes_loop.len()
-}
-
-//bruteforce
-pub fn part2(raw_input: &str) -> Output {
-    let (mut grid_map, (sr, sc), numrows, numcols) = parse(raw_input);
-    let (mut cr, mut cc) = (sr, sc);
-    let (mut dr, mut dc) = match (grid_map.get(&(cr, cc)).unwrap()) {
-        '^' => (-1isize, 0),
-        '>' => (0, 1),
-        'v' => (1, 0),
-        '<' => (0, -1isize),
-        _ => panic!("illegal dir"),
-    };
-    let (sdr, sdc) = (dr, dc);
-
-    grid_map.entry((cr, cc)).insert_entry('.');
-    let mut causes_loop = HashSet::new();
-    for rownum in 0..numrows.try_into().unwrap() {
-        for colnum in 0..numcols.try_into().unwrap() {
-            if (rownum, colnum) != (sr, sc) && *grid_map.get(&(rownum, colnum)).unwrap() == '.' {
-                grid_map.entry((rownum, colnum)).insert_entry('#');
-                (cr, cc) = (sr, sc);
-                (dr, dc) = (sdr, sdc);
-                let mut visited = HashSet::new();
-                'outer: while grid_map.contains_key(&(cr, cc)) {
-                    if visited.contains(&(cr, cc, dr, dc)) {
-                        causes_loop.insert((rownum, colnum));
-                        break 'outer;
-                    }
-                    visited.insert((cr, cc, dr, dc));
-                    let (nr, nc) = (cr + dr, cc + dc);
-                    if !grid_map.contains_key(&(nr, nc)) {
-                        break;
-                    }
-                    let at_loc = grid_map.get(&(nr, nc)).unwrap();
-                    if *at_loc == '.' {
-                        (cr, cc) = (nr, nc);
-                        continue;
-                    } else if *at_loc == '#' {
-                        (dr, dc) = rot((dr, dc))
-                    } else {
-                        panic!("illegal grid char")
-                    }
-                }
-                grid_map.entry((rownum, colnum)).insert_entry('.');
-            }
-        }
-    }
-
-    // println!("causes loop = {:?}", causes_loop);
     causes_loop.len()
 }
